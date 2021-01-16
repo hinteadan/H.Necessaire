ecd8bcf6d2f341033a18761f82ed237cf37216d6
Hintee <hintea_dan@yahoo.co.uk>
2021-01-16 11:26:09 +0200

    OD: Create library

---
+++ Src/H.Necessaire/Testicles/H.Necessaire.Testicles.Unit/DataExtensionsScenarios.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using H.Necessaire;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace H.Necessaire.Testicles.Unit
+{
+    public class DataExtensionsScenarios
+    {
+        [Fact(DisplayName = "Batch returns NULL if the given collection is NULL")]
+        public void Batch_Returns_Null_If_Collection_Is_Null()
+        {
+            (null as IEnumerable<int>).Batch(5).Should().BeNull("the given collection is NULL");
+        }
+
+        [Fact(DisplayName = "Batch returns empty collection if the given collection is empty")]
+        public void Batch_Returns_Empty_If_Collection_Is_Empty()
+        {
+            Enumerable.Empty<int>().Batch(5).Should().BeEmpty("the given collection is NULL");
+        }
+
+        [Fact(DisplayName = "Batch throws invalid operation exception if the given batch size is smaller than 1")]
+        public void Batch_Throws_IvalidOperationException_If_BatchSize_Is_Smaller_Than_One()
+        {
+            new Action(() => new int[] { 1 }.Batch(0)).Should().Throw<InvalidOperationException>("the batch size is zero");
+            new Action(() => new int[] { 1 }.Batch(-3)).Should().Throw<InvalidOperationException>("the batch size is negative");
+        }
+
+        [Fact(DisplayName = "Batch returns the collection itself if the given batch size is greater than or equal to the collection count")]
+        public void Batch_Returns_Self_If_Batch_Size_Is_Greater_Than_Or_Equal_To_Collection_Count()
+        {
+            int[] collection = new int[] { 1, 2, 3, 4, 5 };
+
+            collection.Batch(10).Should().HaveCount(1, "the batch size is greater than the collection count");
+            collection.Batch(10).Single().Should().Equal(collection, "the batch is self as the batch size is greater than the collection count");
+
+            collection.Batch(collection.Length).Should().HaveCount(1, "the batch size is equal to the collection count");
+            collection.Batch(collection.Length).Single().Should().Equal(collection, "the batch is self as the batch size is equal to the collection count");
+        }
+
+        [Fact(DisplayName = "Batch creates expected batches when all params are ok")]
+        public void Batch_Creates_Expected_Batches_If_All_Params_Are_Correct()
+        {
+            int[] collection = new int[] { 1, 2, 3, 4, 5 };
+
+            IEnumerable<IEnumerable<int>> batches = collection.Batch(2);
+
+            batches.Should().HaveCount(3, "we should have 2 complete batches and 1 incomplete");
+            batches.ElementAt(0).Should().BeEquivalentTo(1, 2);
+            batches.ElementAt(1).Should().BeEquivalentTo(3, 4);
+            batches.ElementAt(2).Should().BeEquivalentTo(5);
+        }
+    }
+}
---
+++ Src/H.Necessaire/Testicles/H.Necessaire.Testicles.Unit/ExecutionUtilitiesScenarios.cs
@@ -0,0 +1,239 @@
+﻿using FluentAssertions;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace H.Necessaire.Testicles.Unit
+{
+    public class ExecutionUtilitiesScenarios
+    {
+        private const double timingToleranceInMilliseconds = 80;
+
+        [Fact(DisplayName = "TryAFewTimesOrFailWithGrace Doesnt Retry If Call Succeeds")]
+        public void TryAFewTimesOrFailWithGrace_Doesnt_Retry_If_Call_Succeeds()
+        {
+            int callCount = 0;
+            int retryCallCount = 0;
+            int failCallCount = 0;
+
+            new Action(() =>
+            {
+                callCount++;
+            })
+            .TryOrFailWithGrace(
+                numberOfTimes: 5,
+                onFail: x => failCallCount++,
+                onRetry: x => retryCallCount++,
+                millisecondsToSleepBetweenRetries: 0
+            );
+
+            callCount.Should().Be(1);
+            failCallCount.Should().Be(0);
+            retryCallCount.Should().Be(0);
+        }
+
+        [Fact(DisplayName = "TryAFewTimesOrFailWithGrace Async Awaits and Doesnt Retry If Call Succeeds")]
+        public async Task TryAFewTimesOrFailWithGraceAsync_Awaits_And_Doesnt_Retry_If_Call_Succeeds()
+        {
+            int callCount = 0;
+            int retryCallCount = 0;
+            int failCallCount = 0;
+
+            DateTime startedOn = DateTime.Now;
+
+            await new Func<Task>(async () =>
+            {
+                callCount++;
+                await Task.Delay(200);
+            })
+            .TryOrFailWithGrace(
+                numberOfTimes: 5,
+                onFail: x => failCallCount++,
+                onRetry: x => retryCallCount++,
+                millisecondsToSleepBetweenRetries: 0
+            );
+
+            DateTime endedOn = DateTime.Now;
+
+            TimeSpan duration = endedOn - startedOn;
+
+            duration.TotalMilliseconds.Should().BeApproximately(200, timingToleranceInMilliseconds, "The call is awaited");
+            callCount.Should().Be(1);
+            failCallCount.Should().Be(0);
+            retryCallCount.Should().Be(0);
+        }
+
+        [Fact(DisplayName = "TryAFewTimesOrFailWithGrace Calls Retry Action On Retry")]
+        public void TryAFewTimesOrFailWithGrace_Calls_Retry_Action_On_Retry()
+        {
+            int callCount = 0;
+            int retryCallCount = 0;
+            int failCallCount = 0;
+
+            new Action(() =>
+            {
+                callCount++;
+
+                if (callCount <= 2)
+                    throw new InvalidOperationException("Eat a dick, gravity!");
+            })
+            .TryOrFailWithGrace(
+                numberOfTimes: 5,
+                onFail: x => failCallCount++,
+                onRetry: x => retryCallCount++,
+                millisecondsToSleepBetweenRetries: 0
+            );
+
+            callCount.Should().Be(3);
+            failCallCount.Should().Be(0);
+            retryCallCount.Should().Be(2);
+        }
+
+        [Fact(DisplayName = "TryAFewTimesOrFailWithGrace Async Awaits and Calls Retry Action On Retry")]
+        public async Task TryAFewTimesOrFailWithGraceAsync_Awaits_And_Calls_Retry_Action_On_Retry()
+        {
+            int callCount = 0;
+            int retryCallCount = 0;
+            int failCallCount = 0;
+
+            DateTime startedOn = DateTime.Now;
+
+            await new Func<Task>(async () =>
+            {
+                await Task.Delay(200);
+
+                callCount++;
+
+                if (callCount <= 2)
+                    throw new InvalidOperationException("Eat a dick, gravity!");
+            })
+            .TryOrFailWithGrace(
+                numberOfTimes: 5,
+                onFail: x => failCallCount++,
+                onRetry: x => retryCallCount++,
+                millisecondsToSleepBetweenRetries: 0
+            );
+
+            DateTime endedOn = DateTime.Now;
+
+            TimeSpan duration = endedOn - startedOn;
+
+            duration.TotalMilliseconds.Should().BeApproximately(200 * 3, timingToleranceInMilliseconds, "The call is awaited");
+            callCount.Should().Be(3);
+            failCallCount.Should().Be(0);
+            retryCallCount.Should().Be(2);
+        }
+
+        [Fact(DisplayName = "TryAFewTimesOrFailWithGrace Calls Fail Action On Fail")]
+        public void TryAFewTimesOrFailWithGrace_Calls_Fail_Action_On_Fail()
+        {
+            int callCount = 0;
+            int retryCallCount = 0;
+            int failCallCount = 0;
+
+            new Action(() =>
+            {
+                callCount++;
+                throw new InvalidOperationException("Eat a dick, gravity!");
+            })
+            .TryOrFailWithGrace(
+                numberOfTimes: 5,
+                onFail: x => failCallCount++,
+                onRetry: x => retryCallCount++,
+                millisecondsToSleepBetweenRetries: 0
+            );
+
+            callCount.Should().Be(5);
+            failCallCount.Should().Be(1);
+            retryCallCount.Should().Be(4);
+        }
+
+        [Fact(DisplayName = "TryAFewTimesOrFailWithGrace Async Awaits and Calls Fail Action On Fail")]
+        public async Task TryAFewTimesOrFailWithGraceAsync_Awaits_And_Calls_Fail_Action_On_Fail()
+        {
+            int callCount = 0;
+            int retryCallCount = 0;
+            int failCallCount = 0;
+
+            DateTime startedOn = DateTime.Now;
+
+            await new Func<Task>(async () =>
+            {
+                await Task.Delay(100);
+                callCount++;
+                throw new InvalidOperationException("Eat a dick, gravity!");
+            })
+            .TryOrFailWithGrace(
+                numberOfTimes: 5,
+                onFail: x => failCallCount++,
+                onRetry: x => retryCallCount++,
+                millisecondsToSleepBetweenRetries: 0
+            );
+
+            DateTime endedOn = DateTime.Now;
+
+            TimeSpan duration = endedOn - startedOn;
+
+            duration.TotalMilliseconds.Should().BeApproximately(100 * 5, timingToleranceInMilliseconds * 1.75, "The call is awaited");
+            callCount.Should().Be(5);
+            failCallCount.Should().Be(1);
+            retryCallCount.Should().Be(4);
+        }
+
+        [Fact(DisplayName = "TryAFewTimesOrFailWithGrace Fail And Retry Actions Dont Break Execution No Matter What")]
+        public void TryAFewTimesOrFailWithGrace_Fail_And_Retry_Actions_Dont_Break_Execution_No_Matter_What()
+        {
+            int callCount = 0;
+            int retryCallCount = 0;
+            int failCallCount = 0;
+
+            new Action(() =>
+            {
+                callCount++;
+                throw new InvalidOperationException("Eat a dick, gravity!");
+            })
+            .TryOrFailWithGrace(
+                numberOfTimes: 5,
+                onFail: x => { failCallCount++; throw new InvalidOperationException("Eat more dick, gravity!"); },
+                onRetry: x => { retryCallCount++; throw new InvalidOperationException("Eat more dick, gravity!"); },
+                millisecondsToSleepBetweenRetries: 0
+            );
+
+            callCount.Should().Be(5);
+            failCallCount.Should().Be(1);
+            retryCallCount.Should().Be(4);
+        }
+
+        [Fact(DisplayName = "TryAFewTimesOrFailWithGrace Async Awaits And Fail And Retry Actions Dont Break Execution No Matter What")]
+        public async Task TryAFewTimesOrFailWithGraceAsync_Awaits_And_Fail_And_Retry_Actions_Dont_Break_Execution_No_Matter_What()
+        {
+            int callCount = 0;
+            int retryCallCount = 0;
+            int failCallCount = 0;
+
+            DateTime startedOn = DateTime.Now;
+
+            await new Func<Task>(async () =>
+            {
+                await Task.Delay(100);
+                callCount++;
+                throw new InvalidOperationException("Eat a dick, gravity!");
+            })
+            .TryOrFailWithGrace(
+                numberOfTimes: 5,
+                onFail: x => { failCallCount++; throw new InvalidOperationException("Eat more dick, gravity!"); },
+                onRetry: x => { retryCallCount++; throw new InvalidOperationException("Eat more dick, gravity!"); },
+                millisecondsToSleepBetweenRetries: 0
+            );
+
+            DateTime endedOn = DateTime.Now;
+
+            TimeSpan duration = endedOn - startedOn;
+
+            duration.TotalMilliseconds.Should().BeApproximately(100 * 5, timingToleranceInMilliseconds * 2, "The call is awaited");
+            callCount.Should().Be(5);
+            failCallCount.Should().Be(1);
+            retryCallCount.Should().Be(4);
+        }
+    }
+}
