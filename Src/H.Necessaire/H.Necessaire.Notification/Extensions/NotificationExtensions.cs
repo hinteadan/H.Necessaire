8d136b2081136b6f182801982d8ccc317f330da7
Hintee <hintea_dan@yahoo.co.uk>
2021-01-28 00:29:41 +0200

    OD: Concrete Notifications

---
+++ Src/H.Necessaire/H.Necessaire.Notification/Extensions/NotificationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace H.Necessaire.Notification
+{
+    public static class NotificationExtensions
+    {
+        public static NotificationMessage ToJsonNotificationMessage(this object payload)
+        {
+            return
+                JsonConvert
+                    .SerializeObject(payload, Formatting.None)
+                    .ToNotificationMessage(
+                        subject: null,
+                        NotificationMessageContentType.JSON
+                    );
+        }
+    }
+}
--- Src/H.Necessaire/H.Necessaire.Notification/Http/HttpNotifier.cs
+++ Src/H.Necessaire/H.Necessaire.Notification/Http/HttpNotifier.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace H.Necessaire.Notification
@@ -15,28 +15,93 @@ namespace H.Necessaire.Notification
         {
             OperationResult result = OperationResult.Win();
 
-            //await
-            //    new Func<Task>(async () =>
-            //    {
-            //        using (StringContent httpBody = new StringContent(message.Content, Encoding.UTF8, "application/json"))
-            //        using (HttpResponseMessage response = await http.PostAsync(config.PostUrl, httpBody))
-            //        {
-            //            if (!response.IsSuccessStatusCode)
-            //            {
-            //                result = OperationResult.Fail($"{response.StatusCode}: {response.ReasonPhrase ?? "[NoStatusReason]"}", await response.Content.ReadAsStringAsync());
-            //                return;
-            //            }
-            //        }
-
-            //        result = OperationResult.Win();
-
-            //    })
-            //    .TryOrFailWithGrace(
-            //        numberOfTimes: 3,
-            //        onFail: ex => result = OperationResult.Fail(ex)
-            //    );
+            await
+                new Func<Task>(async () =>
+                {
+                    using (StringContent httpBody = new StringContent(message.Content, message.Encoding, MapMimeType(message.ContentType)))
+                    {
+                        result = await DoHttpPost(httpBody, to);
+                    }
+                })
+                .TryOrFailWithGrace(
+                    numberOfTimes: 1,
+                    onFail: ex => result = OperationResult.Fail(ex)
+                );
 
             return result;
+        }
+
+        private async Task<OperationResult> DoHttpPost(StringContent httpBody, NotificationEndpoint[] to)
+        {
+            if (!to?.Any() ?? true)
+                return OperationResult.Win().WithPayload(new HttpResponseMessage[0]);
+
+            OperationResult[] results
+                = await
+                    Task.WhenAll(
+                        to.Select(x => DoHttpPost(httpBody, x)).ToArray()
+                    );
+
+            bool isSuccess = results.All(x => x.IsSuccessful);
+            string[] reasons = results.SelectMany(x => x.FlattenReasons()).ToArray();
+
+
+            return
+                isSuccess
+                ? OperationResult.Win(reason: null, reasons)
+                : OperationResult.Fail(reason: "See comments for failure details", reasons)
+                ;
+        }
+
+        private async Task<OperationResult> DoHttpPost(StringContent httpBody, NotificationEndpoint to)
+        {
+            OperationResult result = OperationResult.Win();
+
+            await
+                new Func<Task>(async () =>
+                {
+                    using (HttpResponseMessage response = await http.PostAsync(to.Address.Address, httpBody))
+                    {
+                        if (!response.IsSuccessStatusCode)
+                        {
+                            string reason = $"{response.StatusCode}: {response.ReasonPhrase ?? "[NoStatusReason]"}";
+                            string responseContent = await response.Content.ReadAsStringAsync();
+
+                            result
+                                = to.IsOptional
+                                ? OperationResult.Win($"{to.Address} is optional", reason, responseContent)
+                                : OperationResult.Fail(reason, responseContent)
+                                ;
+
+                            return;
+                        }
+                    }
+
+                    result = OperationResult.Win();
+
+                })
+                .TryOrFailWithGrace(
+                    numberOfTimes: 3,
+                    onFail: ex => result = to.IsOptional ? OperationResult.Win($"{to.Address} is optional", ex.Flatten().Select(x => x.ToString()).ToArray()) : OperationResult.Fail(ex)
+                );
+
+            return result;
+        }
+
+        private string MapMimeType(NotificationMessageContentType contentType)
+        {
+            switch (contentType)
+            {
+                case NotificationMessageContentType.Html: return "text/html";
+                case NotificationMessageContentType.JSON: return "application/json";
+                case NotificationMessageContentType.LaTeX: return "application/x-latex";
+                case NotificationMessageContentType.OpenXML: return "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+                case NotificationMessageContentType.XML: return "application/xml";
+                case NotificationMessageContentType.Plain:
+                case NotificationMessageContentType.Markdown:
+                default:
+                    return "text/plain";
+            }
         }
     }
 }
