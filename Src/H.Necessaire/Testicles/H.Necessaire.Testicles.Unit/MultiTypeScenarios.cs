ccd34c5b7bdd1d97a0732cb406b77552e99b7cc0
Hintee <hintea_dan@yahoo.co.uk>
2021-07-20 09:28:48 +0300

    Write MultiType Composition Scenarios

--- Src/H.Necessaire/Testicles/H.Necessaire.Testicles.Unit/MultiTypeScenarios.cs
+++ Src/H.Necessaire/Testicles/H.Necessaire.Testicles.Unit/MultiTypeScenarios.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using System;
 using Xunit;
 
 namespace H.Necessaire.Testicles.Unit
@@ -18,6 +19,21 @@ namespace H.Necessaire.Testicles.Unit
             multiTypeB.ToString().Should().Be("hintee");
             multiTypeA.ToObject().Should().Be(10);
             multiTypeB.ToObject().Should().Be("hintee");
+        }
+
+        [Fact(DisplayName = "MultiType Compositions Work As Expected")]
+        public void MultiType_Compositions_Work_As_Expected()
+        {
+            MultiType<int, MultiType<DateTime, TimeSpan>> composedMultitype = (MultiType<DateTime, TimeSpan>)TimeSpan.FromMilliseconds(10);
+
+            composedMultitype.ToObject().Should().Be(TimeSpan.FromMilliseconds(10));
+            composedMultitype.Read(readSecondType: x => x.Read(readSecondType: y => y.Should().Be(TimeSpan.FromMilliseconds(10))));
+
+
+            composedMultitype = 17;
+
+            composedMultitype.ToObject().Should().Be(17);
+            composedMultitype.Read(readFirstType: x => x.Should().Be(17));
         }
     }
 }
