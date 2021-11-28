35768ec23288fb235a4f789380e548d36fce6435
Hintee <hintea_dan@yahoo.co.uk>
2021-11-28 13:12:24 +0200

    Make BrandingStyle customizable

--- Src/H.Necessaire/H.Necessaire/Models/Branding/BrandingStyle.cs
+++ Src/H.Necessaire/H.Necessaire/Models/Branding/BrandingStyle.cs
@@ -5,35 +5,35 @@
         public static readonly BrandingStyle Default = new BrandingStyle();
 
         #region Construct
-        public readonly int SizingUnitInPixels = 10;
+        public virtual int SizingUnitInPixels { get;  } = 10;
 
         //public ColorPalette Colors { get; } = ColorPalette.Default;
-        public ColorPalette Colors { get; } = ColorPalette.CyanMate;
+        public virtual ColorPalette Colors { get;  } = ColorPalette.CyanMate;
 
         //public Typography Typography { get; } = Typography.Default;
-        public Typography Typography { get; } = Typography.FiraSans;
+        public virtual Typography Typography { get; } = Typography.FiraSans;
         #endregion
 
-        public ColorInfo BackgroundColor => Colors.Complementary.Lighter(10);
-        public ColorInfo BackgroundColorTranslucent => Colors.Complementary.Lighter(10).Clone().And(x => x.Opacity = .83f);
+        public virtual ColorInfo BackgroundColor => Colors.Complementary.Lighter(10);
+        public virtual ColorInfo BackgroundColorTranslucent => Colors.Complementary.Lighter(10).Clone().And(x => x.Opacity = .83f);
 
-        public ColorInfo PrimaryColor => Colors.Primary.Color;
-        public ColorInfo PrimaryColorTranslucent => Colors.Primary.Lighter();
-        public ColorInfo PrimaryColorFaded => Colors.Primary.Lighter(2);
+        public virtual ColorInfo PrimaryColor => Colors.Primary.Color;
+        public virtual ColorInfo PrimaryColorTranslucent => Colors.Primary.Lighter();
+        public virtual ColorInfo PrimaryColorFaded => Colors.Primary.Lighter(2);
 
-        public ColorInfo SecondaryColor => Colors.PrimaryIsh().Color;
-        public ColorInfo SecondaryColorTranslucent => Colors.PrimaryIsh().Lighter();
-        public ColorInfo SecondaryColorFaded => Colors.PrimaryIsh().Lighter(2);
+        public virtual ColorInfo SecondaryColor => Colors.PrimaryIsh().Color;
+        public virtual ColorInfo SecondaryColorTranslucent => Colors.PrimaryIsh().Lighter();
+        public virtual ColorInfo SecondaryColorFaded => Colors.PrimaryIsh().Lighter(2);
 
-        public ColorInfo TextColor { get; } = new ColorInfo(0, 0, 0);
-        public ColorInfo LightTextColor { get; } = new ColorInfo(255, 255, 255, .85f);
-        public ColorInfo MutedTextColor { get; } = new ColorInfo(0, 0, 0, .35f);
-        public ColorInfo HighlightTextColor => Colors.Primary.Darker(10);
+        public virtual ColorInfo TextColor { get; } = new ColorInfo(0, 0, 0);
+        public virtual ColorInfo LightTextColor { get; } = new ColorInfo(255, 255, 255, .85f);
+        public virtual ColorInfo MutedTextColor { get; } = new ColorInfo(0, 0, 0, .35f);
+        public virtual ColorInfo HighlightTextColor => Colors.Primary.Darker(10);
 
-        public ColorInfo SuccessColor { get; } = new ColorInfo("#0b6b31");
-        public ColorInfo WarningColor { get; } = new ColorInfo("#b36f1d");
-        public ColorInfo DangerColor { get; } = new ColorInfo("#911717");
-        public ColorInfo InformationColor { get; } = new ColorInfo("#0e6355");
-        public ColorInfo MuteColor { get; } = new ColorInfo(0, 0, 0, .5f);
+        public virtual ColorInfo SuccessColor { get; } = new ColorInfo("#0b6b31");
+        public virtual ColorInfo WarningColor { get; } = new ColorInfo("#b36f1d");
+        public virtual ColorInfo DangerColor { get; } = new ColorInfo("#911717");
+        public virtual ColorInfo InformationColor { get; } = new ColorInfo("#0e6355");
+        public virtual ColorInfo MuteColor { get; } = new ColorInfo(0, 0, 0, .5f);
     }
 }
