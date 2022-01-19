c2fc185b77d4357f50e5100bbbd3f9e7f62e4cd4
hintee <hintea_dan@yahoo.co.uk>
2022-01-19 22:34:56 +0200

    Add DeepClone method to UI

--- Src/H.Necessaire/H.Necessaire.BridgeDotNet.Runtime.ReactApp/Core/Extensions.cs
+++ Src/H.Necessaire/H.Necessaire.BridgeDotNet.Runtime.ReactApp/Core/Extensions.cs
@@ -6,5 +6,6 @@
         public static T JsonStringToType<T>(this string jsonString) => jsonString.DeserializeFromJson<T>();
         public static object ToJson<T>(this T obj) => obj.ObjectToJson();
         public static T ToType<T>(this object json) => json.JsonToObject<T>();
+        public static T DeepClone<T>(this T data) => data.JsonToObject<T>();
     }
 }
