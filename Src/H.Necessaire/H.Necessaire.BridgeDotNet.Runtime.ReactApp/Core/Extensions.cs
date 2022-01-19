0e921c1633f4bae7f17fb333cd8a51143581349c
hintee <hintea_dan@yahoo.co.uk>
2022-01-19 11:20:46 +0200

    Small correction for UI JSON serialization operations

--- Src/H.Necessaire/H.Necessaire.BridgeDotNet.Runtime.ReactApp/Core/Extensions.cs
+++ Src/H.Necessaire/H.Necessaire.BridgeDotNet.Runtime.ReactApp/Core/Extensions.cs
@@ -2,7 +2,7 @@
 {
     public static class Extensions
     {
-        public static object ToJsonString<T>(this T obj) => obj.SerializeToJson();
+        public static string ToJsonString<T>(this T obj) => obj.SerializeToJson();
         public static T JsonStringToType<T>(this string jsonString) => jsonString.DeserializeFromJson<T>();
         public static object ToJson<T>(this T obj) => obj.ObjectToJson();
         public static T ToType<T>(this object json) => json.JsonToObject<T>();
