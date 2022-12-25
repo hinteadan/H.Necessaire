50bc69d77956c4e8bad45bf0c796ff8da2086e23
Hintee <hintea_dan@yahoo.co.uk>
2022-12-26 00:49:40 +0200

    Add DataBin model for BLOB operations

---
+++ Src/H.Necessaire/H.Necessaire/Models/WellKnownDataBinFormat.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace H.Necessaire
+{
+    public static class WellKnownDataBinFormat
+    {
+        public static readonly DataBinFormatInfo
+            GenericByteStream = new DataBinFormatInfo
+            {
+                ID = nameof(GenericByteStream),
+                Extension = null,
+                MimeType = "application/octet-stream",
+                Encoding = null,
+            };
+
+        public static readonly DataBinFormatInfo
+            PlainTextUTF8 = new DataBinFormatInfo
+            {
+                ID = nameof(PlainTextUTF8),
+                Extension = "txt",
+                MimeType = "text/plain",
+                Encoding = "utf-8",
+            };
+
+        public static readonly DataBinFormatInfo
+            JsonUTF8 = new DataBinFormatInfo
+            {
+                ID = nameof(JsonUTF8),
+                Extension = "json",
+                MimeType = "application/json",
+                Encoding = "utf-8",
+            };
+
+        public static DataBinFormatInfo Default => GenericByteStream;
+    }
+}
