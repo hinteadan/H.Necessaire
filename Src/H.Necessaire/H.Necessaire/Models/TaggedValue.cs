ceb8fb97d2c467b51e2a4879a588299d3dc27758
Hintee <hintea_dan@yahoo.co.uk>
2022-12-25 12:06:35 +0200

    Added TaggedValue model

---
+++ Src/H.Necessaire/H.Necessaire/Models/TaggedValue.cs
@@ -0,0 +1,10 @@
+﻿namespace H.Necessaire
+{
+    public class TaggedValue<TValue> : IStringIdentity
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public TValue Value { get; set; }
+    }
+}
