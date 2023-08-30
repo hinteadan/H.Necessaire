831b43148ad8d5d0d4f2cfc5849f2549ba872fe1
Hintee <hintea_dan@yahoo.co.uk>
2023-08-30 17:49:25 +0300

    Implement DiacriticLess

--- Src/H.Necessaire/H.Necessaire/Extensions/DataExtensions.cs
+++ Src/H.Necessaire/H.Necessaire/Extensions/DataExtensions.cs
@@ -10,6 +10,100 @@ namespace H.Necessaire
     public static class DataExtensions
     {
         const string defaultGlobalReasonForMultipleFailedOperations = "There are multiple failure reasons; see comments for details.";
+        static Dictionary<string, string> diacriticsDictionaryBulk = new Dictionary<string, string>
+        {
+            { "äæǽ", "ae" },
+            { "öœ", "oe" },
+            { "ü", "ue" },
+            //{ "Ä", "Ae" },
+            { "Ü", "Ue" },
+            { "Ö", "Oe" },
+            { "ÀÁÂÃÄÅǺĀĂĄǍΑΆẢẠẦẪẨẬẰẮẴẲẶА", "A" },
+            { "àáâãåǻāăąǎªαάảạầấẫẩậằắẵẳặа", "a" },
+            { "Б", "B" },
+            { "б", "b" },
+            { "ÇĆĈĊČ", "C" },
+            { "çćĉċč", "c" },
+            { "Д", "D" },
+            { "д", "d" },
+            { "ÐĎĐΔ", "Dj" },
+            { "ðďđδ", "dj" },
+            { "ÈÉÊËĒĔĖĘĚΕΈẼẺẸỀẾỄỂỆЕЭ", "E" },
+            { "èéêëēĕėęěέεẽẻẹềếễểệеэ", "e" },
+            { "Ф", "F" },
+            { "ф", "f" },
+            { "ĜĞĠĢΓГҐ", "G" },
+            { "ĝğġģγгґ", "g" },
+            { "ĤĦ", "H" },
+            { "ĥħ", "h" },
+            { "ÌÍÎÏĨĪĬǏĮİΗΉΊΙΪỈỊИЫ", "I" },
+            { "ìíîïĩīĭǐįıηήίιϊỉịиыї", "i" },
+            { "Ĵ", "J" },
+            { "ĵ", "j" },
+            { "ĶΚК", "K" },
+            { "ķκк", "k" },
+            { "ĹĻĽĿŁΛЛ", "L" },
+            { "ĺļľŀłλл", "l" },
+            { "М", "M" },
+            { "м", "m" },
+            { "ÑŃŅŇΝН", "N" },
+            { "ñńņňŉνн", "n" },
+            { "ÒÓÔÕŌŎǑŐƠØǾΟΌΩΏỎỌỒỐỖỔỘỜỚỠỞỢО", "O" },
+            { "òóôõōŏǒőơøǿºοόωώỏọồốỗổộờớỡởợо", "o" },
+            { "П", "P" },
+            { "п", "p" },
+            { "ŔŖŘΡР", "R" },
+            { "ŕŗřρр", "r" },
+            { "ŚŜŞȘŠΣС", "S" },
+            { "śŝşșšſσςс", "s" },
+            { "ȚŢŤŦτТ", "T" },
+            { "țţťŧт", "t" },
+            { "ÙÚÛŨŪŬŮŰŲƯǓǕǗǙǛỦỤỪỨỮỬỰУ", "U" },
+            { "ùúûũūŭůűųưǔǖǘǚǜυύϋủụừứữửựу", "u" },
+            { "ÝŸŶΥΎΫỲỸỶỴЙ", "Y" },
+            { "ýÿŷỳỹỷỵй", "y" },
+            { "В", "V" },
+            { "в", "v" },
+            { "Ŵ", "W" },
+            { "ŵ", "w" },
+            { "ŹŻŽΖЗ", "Z" },
+            { "źżžζз", "z" },
+            { "ÆǼ", "AE" },
+            { "ß", "ss" },
+            { "Ĳ", "IJ" },
+            { "ĳ", "ij" },
+            { "Œ", "OE" },
+            { "ƒ", "f" },
+            { "ξ", "ks" },
+            { "π", "p" },
+            { "β", "v" },
+            { "μ", "m" },
+            { "ψ", "ps" },
+            { "Ё", "Yo" },
+            { "ё", "yo" },
+            { "Є", "Ye" },
+            { "є", "ye" },
+            { "Ї", "Yi" },
+            { "Ж", "Zh" },
+            { "ж", "zh" },
+            { "Х", "Kh" },
+            { "х", "kh" },
+            { "Ц", "Ts" },
+            { "ц", "ts" },
+            { "Ч", "Ch" },
+            { "ч", "ch" },
+            { "Ш", "Sh" },
+            { "ш", "sh" },
+            { "Щ", "Shch" },
+            { "щ", "shch" },
+            { "ЪъЬь", "" },
+            { "Ю", "Yu" },
+            { "ю", "yu" },
+            { "Я", "Ya" },
+            { "я", "ya" },
+        };
+        static Dictionary<char, string> diacriticsDictionary = diacriticsDictionaryBulk.SelectMany(kvp => kvp.Key.Select(d => new KeyValuePair<char, string>(d, kvp.Value))).ToDictionary(x => x.Key, x => x.Value);
+
 
         public static DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
 
@@ -439,6 +533,27 @@ namespace H.Necessaire
             geoAddress.Notes = geoAddress.Notes?.Where(n => !n.IsEmpty()).ToArrayNullIfEmpty();
 
             return geoAddress;
+        }
+
+        public static string DiacriticLess(this string value)
+        {
+            if (value.IsEmpty())
+                return value;
+
+            StringBuilder resultBuilder = new StringBuilder(value.Length);
+
+            foreach (char c in value)
+            {
+                if (diacriticsDictionary.ContainsKey(c) == false)
+                {
+                    resultBuilder.Append(c);
+                    continue;
+                }
+
+                resultBuilder.Append(diacriticsDictionary[c]);
+            }
+
+            return resultBuilder.ToString();
         }
     }
 }
