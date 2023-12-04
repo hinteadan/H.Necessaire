0767266a2818f73a5a33dd6d852154599edfbbf4
Hintee <hintea_dan@yahoo.co.uk>
2023-12-04 11:34:53 +0200

    Implement AuditFileSystemStorageResource Browse

--- Src/H.Necessaire/H.Necessaire.Runtime/Resources/Concrete/AuditFileSystemStorageResource.cs
+++ Src/H.Necessaire/H.Necessaire.Runtime/Resources/Concrete/AuditFileSystemStorageResource.cs
@@ -52,7 +52,50 @@ namespace H.Necessaire.Runtime.Resources.Concrete
 
         protected override IEnumerable<ImAnAuditEntry> ApplyFilter(IEnumerable<ImAnAuditEntry> stream, AuditSearchFilter filter)
         {
-            throw new NotImplementedException("This should not be called");
+            IEnumerable<ImAnAuditEntry> result = stream;
+
+            if (filter?.IDs?.Any() ?? false)
+            {
+                result = result.Where(x => x.ID.In(filter.IDs));
+            }
+
+            if (filter?.AuditedObjectTypes?.Any() ?? false)
+            {
+                result = result.Where(x => x.AuditedObjectType.In(filter.AuditedObjectTypes));
+            }
+
+            if (filter?.AuditedObjectIDs?.Any() ?? false)
+            {
+                result = result.Where(x => x.AuditedObjectID.In(filter.AuditedObjectIDs));
+            }
+
+            if (filter?.ActionTypes?.Any() ?? false)
+            {
+                result = result.Where(x => x.ActionType.In(filter.ActionTypes));
+            }
+
+            if (filter?.DoneBy != null)
+            {
+                result = result.Where(x =>
+                    ((filter.DoneBy?.IDs?.Any() ?? false) ? (x.DoneBy?.ID.In(filter.DoneBy.IDs) ?? false) : true)
+                    || ((filter.DoneBy?.IDTags?.Any() ?? false) ? (x.DoneBy?.IDTag?.In(filter.DoneBy.IDTags) ?? false) : true)
+                    || ((filter.DoneBy?.DisplayNames?.Any() ?? false) ? (x.DoneBy?.DisplayName?.In(filter.DoneBy.DisplayNames) ?? false) : true)
+                );
+            }
+
+            if (filter?.FromInclusive != null)
+            {
+                result = result.Where(x => x.HappenedAt >= filter.FromInclusive.Value);
+            }
+
+            if (filter?.ToInclusive != null)
+            {
+                result = result.Where(x => x.HappenedAt <= filter.ToInclusive.Value);
+            }
+
+            result = result.ApplySortAndPageFilterIfAny(filter);
+
+            return result;
         }
 
         private ImAnAuditEntry BuildAuditEntry(ImAnAuditEntry x)
