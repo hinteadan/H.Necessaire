d859fa8337844b8a7297811e63a1c0392d0f8608
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-02-28 10:30:06 +0200

    OD: UI Base components

---
+++ Src/H.Necessaire/Apps/H.Necessaire.ReactAppSample/Components/DataEditComponent.cs
@@ -0,0 +1,14 @@
+﻿using Bridge;
+using Bridge.React;
+using H.Necessaire.BridgeDotNet.Runtime.ReactApp;
+
+namespace H.Necessaire.ReactAppSample.Components
+{
+    internal class DataEditComponent : DataEditComponentBase<string, DataEditComponent.Props, DataEditComponent.State>
+    {
+        public DataEditComponent(Props props, params Union<ReactElement, string>[] children) : base(props, children) { }
+
+        public class State : ComponentStateBase { }
+        public class Props : ComponentPropsBase { }
+    }
+}
