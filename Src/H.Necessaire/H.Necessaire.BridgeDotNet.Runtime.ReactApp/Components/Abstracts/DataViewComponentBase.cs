6213b0a5c3000c32b5db8f026dd5d0cf3088eeac
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-02-28 10:22:24 +0200

    OD: UI Base components

---
+++ Src/H.Necessaire/H.Necessaire.BridgeDotNet.Runtime.ReactApp/Components/Abstracts/DataViewComponentBase.cs
@@ -0,0 +1,15 @@
+﻿using Bridge;
+using Bridge.Html5;
+using Bridge.React;
+using H.Necessaire.Models.Branding;
+using H.Necessaire.UI;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace H.Necessaire.BridgeDotNet.Runtime.ReactApp
+{
+    public abstract class DataViewComponentBase
+    {
+    }
+}
