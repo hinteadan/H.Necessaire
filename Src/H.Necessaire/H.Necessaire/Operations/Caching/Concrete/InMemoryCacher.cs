873ceffa19b7c220021992875ca9278a43773ed0
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-03-05 10:48:07 +0200

    OD: UI Base components

--- Src/H.Necessaire/H.Necessaire/Operations/Caching/Concrete/InMemoryCacher.cs
+++ Src/H.Necessaire/H.Necessaire/Operations/Caching/Concrete/InMemoryCacher.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Threading.Tasks;
 
 namespace H.Necessaire.Operations.Caching.Concrete
 {
     internal class InMemoryCacher<T> : ImACacher<T>
     {
+        readonly ConcurrentDictionary<string, ImCachebale<T>> cacheRegistry = new ConcurrentDictionary<string, ImCachebale<T>>();
+
         public Task<T> GetOrAdd(string id, Func<string, Task<ImCachebale<T>>> cacheableItemFactory)
         {
             throw new NotImplementedException();
