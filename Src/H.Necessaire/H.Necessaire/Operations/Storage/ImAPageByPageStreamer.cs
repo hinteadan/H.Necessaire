27c4a0209e39d4d35ef23c08953e06b70d1e943b
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-03-07 12:56:57 +0200

    OD: UI Base components

---
+++ Src/H.Necessaire/H.Necessaire/Operations/Storage/ImAPageByPageStreamer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace H.Necessaire
+{
+    public interface ImAPageByPageStreamer<TEntity>
+    {
+        IEnumerable<Page<TEntity>> StreamAllPageByPage(int pageSize = 25);
+    }
+}
---
+++ Src/H.Necessaire/H.Necessaire/Operations/Storage/ImAnAttachmentStore.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace H.Necessaire
+{
+    public interface ImAnAttachmentStore<TEntityID>
+    {
+        Task<OperationResult> SaveAttachment(TEntityID entityID, DataBin attachment);
+        Task<OperationResult<DataBin[]>> LoadAllAttachments(TEntityID entityID);
+    }
+}
