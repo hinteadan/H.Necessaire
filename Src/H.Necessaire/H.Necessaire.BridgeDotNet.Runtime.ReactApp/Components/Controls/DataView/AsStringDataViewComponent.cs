2d94e145191f0b805273edd01ce2ab3a3b30792a
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-03-08 15:39:26 +0200

    OD: UI Base components

---
+++ Src/H.Necessaire/H.Necessaire.BridgeDotNet.Runtime.ReactApp/Components/Controls/DataView/AsStringDataViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Bridge;
+using Bridge.React;
+
+namespace H.Necessaire.BridgeDotNet.Runtime.ReactApp
+{
+    public class AsStringDataViewComponent : DataViewComponentBase<string, AsStringDataViewComponent.Props, AsStringDataViewComponent.State>
+    {
+        public AsStringDataViewComponent(Props props, params Union<ReactElement, string>[] children) : base(props, children) { }
+
+        public class State : DataViewComponentState<string>
+        { 
+
+        }
+
+        public class Props : DataViewComponentProps<string>
+        {
+
+        }
+    }
+}
