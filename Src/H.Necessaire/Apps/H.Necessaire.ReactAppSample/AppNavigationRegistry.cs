7a4fbe680f28075e6637c4859a9d7985864634e2
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-03-15 17:46:12 +0200

    OD: UI Base components

--- Src/H.Necessaire/Apps/H.Necessaire.ReactAppSample/AppNavigationRegistry.cs
+++ Src/H.Necessaire/Apps/H.Necessaire.ReactAppSample/AppNavigationRegistry.cs
@@ -2,7 +2,9 @@
 using H.Necessaire.BridgeDotNet.Runtime.ReactApp;
 using H.Necessaire.ReactAppSample.Pages;
 using ProductiveRage.Immutable;
+using ProductiveRage.ReactRouting;
 using ProductiveRage.ReactRouting.Helpers;
+using System;
 
 namespace H.Necessaire.ReactAppSample
 {
@@ -21,6 +23,13 @@ namespace H.Necessaire.ReactAppSample
                 segments: new NonNullList<string>(AppBase.BaseHostPathParts).Add("home"),
                 routeAction: new AppPageDispatcherAction<HomePage>(),
                 urlGenerator: () => GetPath("home")
+            );
+
+            result = result.AddFor<AppPageDispatcherAction<DataViewPage>>(x => new DataViewPage(new DataViewPage.Props { NavigationParams = x.NavigationParams }));
+            AddRelativeRoute(
+                routeDetails: RouteBuilder.Empty.Fixed("view").Fixed("testdata").String(x => x?.ToString()),
+                routeActionGenerator: id => new AppPageDispatcherAction<DataViewPage>(new UiNavigationParams(id.ParseToGuidOrFallbackTo(Guid.Empty).Value)),
+                urlGenerator: id => GetPath("view", "testdata", id)
             );
 
             return result;
---
+++ Src/H.Necessaire/Apps/H.Necessaire.ReactAppSample/Pages/DataViewPage.cs
@@ -0,0 +1,33 @@
+﻿using Bridge;
+using Bridge.React;
+using H.Necessaire.BridgeDotNet.Runtime.ReactApp;
+using System;
+using System.Threading.Tasks;
+
+namespace H.Necessaire.ReactAppSample.Pages
+{
+    public class DataViewPage : GuidIDDataViewPageBase<TestData, DataViewPage.Props, DataViewPage.State>
+    {
+        public DataViewPage(Props props, params Union<ReactElement, string>[] children) : base(props, children) { }
+
+        protected override async Task<OperationResult<TestData>> LoadData(Guid dataID)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(2.17));
+            return new TestData().ToWinResult();
+        }
+
+
+
+
+        public class State : DataViewPageState<TestData> { }
+
+        public class Props : DataViewPageProps { }
+    }
+
+    public class TestData : IGuidIdentity
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; } = "Hin";
+        public string LastName { get; set; } = "Tee";
+    }
+}
