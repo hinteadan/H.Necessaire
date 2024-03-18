565c0446ab54526ab3bc37fd73f9ae104a6e2b0e
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-03-18 23:22:31 +0200

    OD: ServiceWorker playground

---
+++ Src/H.Necessaire/H.Necessaire.BridgeDotNet.Runtime.ReactApp/Core/ServiceWorker/ServiceWorkerCache.cs
@@ -0,0 +1,42 @@
+﻿using Bridge;
+using static Retyped.dom;
+using static Retyped.es5;
+
+namespace H.Necessaire.BridgeDotNet.Runtime.ReactApp
+{
+    [External]
+    public class ServiceWorkerCache
+    {
+        [External]
+        [Name("add")]
+        public extern Promise<object> Add(dynamic request);
+
+        [External]
+        [Name("put")]
+        public extern Promise<object> Put(dynamic request, dynamic response);
+
+        [External]
+        [Name("addAll")]
+        public extern Promise<object> AddAll(dynamic[] requests);
+
+        //https://developer.mozilla.org/en-US/docs/Web/API/Cache/delete#options
+        [External]
+        [Name("delete")]
+        public extern Promise<bool> Delete(dynamic request, dynamic options = null);
+
+        //https://developer.mozilla.org/en-US/docs/Web/API/Cache/keys#options
+        [External]
+        [Name("keys")]
+        public extern Promise<dynamic[]> Keys(dynamic request = null, dynamic options = null);
+
+        //https://developer.mozilla.org/en-US/docs/Web/API/Cache/match#options
+        [External]
+        [Name("match")]
+        public extern Promise<dynamic> Match(dynamic request, dynamic options = null);
+
+        //https://developer.mozilla.org/en-US/docs/Web/API/Cache/matchAll#options
+        [External]
+        [Name("matchAll")]
+        public extern Promise<dynamic[]> MatchAll(dynamic request = null, dynamic options = null);
+    }
+}
---
+++ Src/H.Necessaire/H.Necessaire.BridgeDotNet.Runtime.ReactApp/Core/ServiceWorker/ServiceWorkerCacheStorage.cs
@@ -0,0 +1,31 @@
+﻿using Bridge;
+using static Retyped.dom;
+using static Retyped.es5;
+
+namespace H.Necessaire.BridgeDotNet.Runtime.ReactApp
+{
+    [External]
+    public class ServiceWorkerCacheStorage
+    {
+        [External]
+        [Name("delete")]
+        public extern Promise<bool> Delete(string cacheName);
+
+        [External]
+        [Name("has")]
+        public extern Promise<bool> Has(string cacheName);
+
+        [External]
+        [Name("keys")]
+        public extern Promise<string[]> Keys(string cacheName);
+
+        //https://developer.mozilla.org/en-US/docs/Web/API/CacheStorage/match#options
+        [External]
+        [Name("match")]
+        public extern Promise<dynamic> Match(dynamic request, dynamic options = null);
+
+        [External]
+        [Name("open")]
+        public extern Promise<ServiceWorkerCache> Open(string cacheName);
+    }
+}
