4ef1e75bed1cc8baeb3f02007351397ef9fa1f02
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-03-18 23:10:49 +0200

    OD: ServiceWorker playground

--- Src/H.Necessaire/H.Necessaire.BridgeDotNet.Runtime.ReactApp/Core/ServiceWorker/ServiceWorkerRegistration.cs
+++ Src/H.Necessaire/H.Necessaire.BridgeDotNet.Runtime.ReactApp/Core/ServiceWorker/ServiceWorkerRegistration.cs
@@ -16,7 +16,41 @@ namespace H.Necessaire.BridgeDotNet.Runtime.ReactApp
         public static extern ServiceWorker InstallingServiceWorker { get; }
 
         [External]
+        [Name("waiting")]
+        public static extern ServiceWorker WaitingServiceWorker { get; }
+
+        [External]
         [Name("navigationPreload")]
         public static extern NavigationPreloadManager NavigationPreloadManager { get; }
+
+        [External]
+        [Name("pushManager")]
+        public static extern dynamic PushManager { get; }
+
+        [External]
+        [Name("scope")]
+        public static extern string Scope { get; }
+
+        [External]
+        [Name("updateViaCache")]
+        public static extern string UpdateViaCache { get; }
+
+        //https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/getNotifications
+        [External]
+        [Name("getNotifications")]
+        public extern Promise<dynamic> GetNotifications(dynamic options = null);
+
+        //https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification
+        [External]
+        [Name("showNotification")]
+        public extern Promise<object> ShowNotification(string title, dynamic options = null);
+
+        [External]
+        [Name("unregister")]
+        public static extern Promise<bool> Unregister();
+
+        [External]
+        [Name("update")]
+        public static extern Promise<ServiceWorkerRegistration> Update();
     }
 }
