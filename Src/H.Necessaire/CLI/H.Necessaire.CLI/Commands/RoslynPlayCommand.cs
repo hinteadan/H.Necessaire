ff79bb2d05079a297abe5c2221788485329d2dcd
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-03-20 13:32:35 +0200

    OD: Roslyn playground

---
+++ Src/H.Necessaire/CLI/H.Necessaire.CLI/Commands/RoslynPlayCommand.cs
@@ -0,0 +1,15 @@
+﻿using H.Necessaire.Runtime.CLI.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace H.Necessaire.CLI.Commands
+{
+    [ID("play-roslyn")]
+    public class RoslynPlayCommand : CommandBase
+    {
+        public override Task<OperationResult> Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
