78d1160f1dfda9a672a951ce09d40831f2da3745
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-03-20 23:19:44 +0200

    OD: Roslyn playground

--- Src/H.Necessaire/CLI/H.Necessaire.CLI/Commands/RoslynPlayCommand.cs
+++ Src/H.Necessaire/CLI/H.Necessaire.CLI/Commands/RoslynPlayCommand.cs
@@ -4,6 +4,7 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -31,7 +32,7 @@ namespace H.Necessaire.CLI.Commands
                 CsFiles = csProj.Directory.GetFiles("*.cs", SearchOption.AllDirectories),
             }).ToArray();
 
-
+            HTypeInfo[] hTypes = projectInfos.SelectMany(ProcessProjectTypes).ToArray();
 
             //FileInfo sourceCodeFile = new FileInfo(@"C:\H\H.Necessaire\Src\H.Necessaire\CLI\H.Necessaire.CLI\Commands\PingCommand.cs");
             //string sourceCode = await sourceCodeFile.OpenRead().ReadAsStringAsync(isStreamLeftOpen: false);
@@ -44,6 +45,82 @@ namespace H.Necessaire.CLI.Commands
 
 
             return OperationResult.Win();
+        }
+
+        private static HTypeInfo[] ProcessProjectTypes(ProjectInfo projectInfo)
+        {
+            if (projectInfo?.CsFiles?.Any() != true)
+                return Array.Empty<HTypeInfo>();
+
+            return
+                projectInfo
+                .CsFiles
+                .SelectMany(
+                    csFile => ProcessCsFile(csFile, projectInfo)
+                )
+                .ToNoNullsArray(nullIfEmpty: false)
+                ;
+        }
+
+        private static HTypeInfo[] ProcessCsFile(FileInfo csFile, ProjectInfo projectInfo)
+        {
+            if(csFile?.Exists != true)
+                return Array.Empty<HTypeInfo>();
+
+            string sourceCode = File.ReadAllText(csFile.FullName);
+            SyntaxTree syntaxTreesyntaxTree = CSharpSyntaxTree.ParseText(sourceCode, CSharpParseOptions.Default);
+            CompilationUnitSyntax root = syntaxTreesyntaxTree.GetCompilationUnitRoot();
+
+            IEnumerable<TypeDeclarationSyntax> allTypeDeclarations = root.DescendantNodes().OfType<TypeDeclarationSyntax>();
+
+            if(allTypeDeclarations.Any() != true)
+                return Array.Empty<HTypeInfo>();
+
+            return
+                allTypeDeclarations
+                .Select(x => ProcessType(x, csFile, sourceCode, projectInfo))
+                .ToNoNullsArray(nullIfEmpty: false)
+                ;
+        }
+
+        private static HTypeInfo ProcessType(TypeDeclarationSyntax typeDeclaration, FileInfo csFile, string sourceCode, ProjectInfo projectInfo)
+        {
+            NamespaceDeclarationSyntax ns = FindNamespaceFor(typeDeclaration);
+            string nsName = ns?.Name.ToString();
+
+            string category 
+                = csFile.Directory.FullName == projectInfo.CsProj.Directory.FullName
+                ? null :
+                csFile.Directory.FullName
+                .Replace(projectInfo.CsProj.Directory.FullName, "")
+                ;
+
+            return
+                new HTypeInfo
+                {
+                    ID = $"{(nsName.IsEmpty() ? "" : $"{nsName}.")}{typeDeclaration.Identifier.Text}",
+                    Module = projectInfo.ID,
+                    Name = typeDeclaration.Identifier.Text,
+                    Namespace = nsName,
+                    Category = category,
+                };
+        }
+
+        private static NamespaceDeclarationSyntax FindNamespaceFor(SyntaxNode syntaxNode)
+        {
+            if (syntaxNode == null)
+                return null;
+
+            if (syntaxNode is NamespaceDeclarationSyntax self)
+                return self;
+
+            if (syntaxNode.Parent is null)
+                return null;
+
+            if (syntaxNode.Parent is NamespaceDeclarationSyntax parent)
+                return parent;
+
+            return FindNamespaceFor(syntaxNode.Parent);
         }
 
         private static string GetCodebaseFolderPath()
@@ -67,5 +144,30 @@ namespace H.Necessaire.CLI.Commands
         public FileInfo[] CsFiles { get; set; }
 
         public override string ToString() => ID;
+    }
+
+    public class HTypeInfo : IStringIdentity
+    {
+        public string ID { get; set; }
+        public string Module { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Namespace { get; set; }
+
+        public HConstructorInfo[] Constructors { get; set; }
+        public HPropertyInfo[] Properties { get; set; }
+        public HMethodInfo[] Methods { get; set; }
+    }
+
+    public class HConstructorInfo
+    {
+    }
+
+    public class HMethodInfo
+    { 
+    }
+
+    public class HPropertyInfo
+    {
     }
 }
