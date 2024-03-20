fe11ec9682026e6275ff62c9c2a68b712fc6f3d9
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-03-20 16:43:57 +0200

    OD: Roslyn playground

--- Src/H.Necessaire/CLI/H.Necessaire.CLI/Commands/RoslynPlayCommand.cs
+++ Src/H.Necessaire/CLI/H.Necessaire.CLI/Commands/RoslynPlayCommand.cs
@@ -1,5 +1,12 @@
 ﻿using H.Necessaire.Runtime.CLI.Commands;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
 using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace H.Necessaire.CLI.Commands
@@ -7,9 +14,41 @@ namespace H.Necessaire.CLI.Commands
     [ID("play-roslyn")]
     public class RoslynPlayCommand : CommandBase
     {
-        public override Task<OperationResult> Run()
+        const string srcFolderRelativePath = "Src/H.Necessaire";
+
+        public override async Task<OperationResult> Run()
         {
-            throw new NotImplementedException();
+            Note[] args = (await GetArguments())?.Jump(1);
+            DirectoryInfo srcFolder = new DirectoryInfo(args?.Get("src") ?? Path.Combine(GetCodebaseFolderPath(), "H.Necessaire"));
+            if (!srcFolder.Exists)
+                return OperationResult.Fail($"Source folder {srcFolder.FullName} doesn't exist");
+
+            FileInfo[] srcFiles = srcFolder.EnumerateFiles("*.cs", SearchOption.AllDirectories).ToArray();
+
+
+            FileInfo sourceCodeFile = new FileInfo(@"C:\H\H.Necessaire\Src\H.Necessaire\CLI\H.Necessaire.CLI\Commands\PingCommand.cs");
+            string sourceCode = await sourceCodeFile.OpenRead().ReadAsStringAsync(isStreamLeftOpen: false);
+            SyntaxTree syntaxTreesyntaxTree = CSharpSyntaxTree.ParseText(sourceCode, CSharpParseOptions.Default);
+            CompilationUnitSyntax root = syntaxTreesyntaxTree.GetCompilationUnitRoot();
+            ClassDeclarationSyntax[] classes 
+                = root.DescendantNodes().OfType<ClassDeclarationSyntax>().ToArrayNullIfEmpty();
+            MethodDeclarationSyntax[] methods
+                = root.DescendantNodes().OfType<MethodDeclarationSyntax>().ToArrayNullIfEmpty();
+
+
+            return OperationResult.Win();
+        }
+
+        private static string GetCodebaseFolderPath()
+        {
+            string codeBase = Assembly.GetExecutingAssembly()?.Location ?? string.Empty;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string dllPath = Uri.UnescapeDataString(uri.Path);
+            int srcFolderIndex = dllPath.ToLowerInvariant().IndexOf(srcFolderRelativePath, StringComparison.InvariantCultureIgnoreCase);
+            if (srcFolderIndex < 0)
+                return string.Empty;
+            string srcFolderPath = Path.GetDirectoryName(dllPath.Substring(0, srcFolderIndex + srcFolderRelativePath.Length)) ?? string.Empty;
+            return srcFolderPath;
         }
     }
 }
