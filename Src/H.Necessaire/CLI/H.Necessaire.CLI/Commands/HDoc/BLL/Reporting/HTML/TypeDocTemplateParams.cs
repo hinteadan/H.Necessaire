f8bfa1943b955fb3a50328055f7f5cb4d3a9fe3a
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-03-26 11:23:50 +0200

    OD: Roslyn playground

---
+++ Src/H.Necessaire/CLI/H.Necessaire.CLI/Commands/HDoc/BLL/Reporting/HTML/TypeDocTemplateParams.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace H.Necessaire.CLI.Commands.HDoc.BLL.Reporting.HTML
+{
+    internal class TypeDocTemplateParams
+    {
+        public string PageTitle { get; set; }
+        public string ContentTitle { get; set; }
+    }
+}
