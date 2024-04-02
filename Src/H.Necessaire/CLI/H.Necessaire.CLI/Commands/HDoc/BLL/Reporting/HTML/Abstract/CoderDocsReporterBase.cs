be6a3ebe6f760d0f6fe305f76626a668351cab29
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-04-02 23:20:57 +0300

    OD: Roslyn playground

--- Src/H.Necessaire/CLI/H.Necessaire.CLI/Commands/HDoc/BLL/Reporting/HTML/Abstract/CoderDocsReporterBase.cs
+++ Src/H.Necessaire/CLI/H.Necessaire.CLI/Commands/HDoc/BLL/Reporting/HTML/Abstract/CoderDocsReporterBase.cs
@@ -6,7 +6,8 @@ namespace H.Necessaire.CLI.Commands.HDoc.BLL.Reporting.HTML.Abstract
 {
     internal abstract class CoderDocsReporterBase<T> : StaticWebSiteReporterBase<T>
     {
-        const string prefix = "H.Necessaire.CLI.Commands.HDoc.BLL.Reporting.HTML.Templates.CoderDocs_BS5_v3._0.assets.";
+        const string templateRootPath = "Commands/HDoc/BLL/Reporting/HTML/Templates/CoderDocs-BS5-v3.0/";
+        const string templateAssetsPath = "Commands/HDoc/BLL/Reporting/HTML/Templates/CoderDocs-BS5-v3.0/assets/";
 
         protected override async Task<IEnumerable<Task<TaggedStream>>> GetContentStreams()
         {
@@ -16,14 +17,15 @@ namespace H.Necessaire.CLI.Commands.HDoc.BLL.Reporting.HTML.Abstract
                 = GetType()
                 .Assembly
                 .GetManifestResourceNames()
-                .Where(x => x.Contains("Templates.CoderDocs_BS5_v3._0.assets"))
+                .Where(x => x.Contains(templateAssetsPath))
                 ;
 
             IEnumerable<Task<TaggedStream>> streams
                 = assetResourceNames
                 .Select(assetResourceName => new TaggedStream
                 {
-                    ID = assetResourceName.Substring(prefix.Length).Replace(".", "/"),
+                    ID = assetResourceName.Substring(templateRootPath.Length),
+                    Name = assetResourceName.Substring(assetResourceName.LastIndexOf('/') + 1),
                     Value = assetResourceName.OpenEmbeddedResource()
                 }.AsTask())
                 ;
