b45a1fef9107f92ffe7b2f95064a4daa7f208a8d
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-04-03 16:35:03 +0300

    OD: Roslyn playground

--- Src/H.Necessaire/CLI/H.Necessaire.CLI/Commands/HDoc/BLL/Reporting/HTML/Concrete/CoderDocs/CoderDocsIndexTemplate.cs
+++ Src/H.Necessaire/CLI/H.Necessaire.CLI/Commands/HDoc/BLL/Reporting/HTML/Concrete/CoderDocs/CoderDocsIndexTemplate.cs
@@ -12,6 +12,7 @@ namespace H.Necessaire.CLI.Commands.HDoc.BLL.Reporting.HTML.Concrete.CoderDocs
         public ContentHeaderPartTemplate ContentHeader { get; set; } = new ContentHeaderPartTemplate();
         public ContentCardPartTemplate[] ContentCards { get; set; } = Array.Empty<ContentCardPartTemplate>();
         public ContentFooterPartTemplate ContentFooter { get; set; } = new ContentFooterPartTemplate();
+        public PageFooterPartTemplate PageFooter { get; set; } = new PageFooterPartTemplate();
 
         public async Task<string> PageHeaderPart()
             => await PageHeader.ProcessEmbeddedResource("CoderDocs-BS5-v3.0/Parts/Index/pageheader.part.tmpl.html");
@@ -24,5 +25,8 @@ namespace H.Necessaire.CLI.Commands.HDoc.BLL.Reporting.HTML.Concrete.CoderDocs
 
         public async Task<string> ContentFooterPart()
             => await ContentFooter.ProcessEmbeddedResource("CoderDocs-BS5-v3.0/Parts/Index/contentfooter.part.tmpl.html");
+
+        public async Task<string> PageFooterPart()
+            => await ContentFooter.ProcessEmbeddedResource("CoderDocs-BS5-v3.0/Parts/Index/pagefooter.part.tmpl.html");
     }
 }
---
+++ Src/H.Necessaire/CLI/H.Necessaire.CLI/Commands/HDoc/BLL/Reporting/HTML/Concrete/CoderDocs/Parts/PageFooterPartTemplate.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace H.Necessaire.CLI.Commands.HDoc.BLL.Reporting.HTML.Concrete.CoderDocs.Parts
+{
+    internal class PageFooterPartTemplate
+    {
+    }
+}
