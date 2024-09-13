957a7b5aed2f81cc5d85006cb071e298b3c0d708
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-09-13 11:45:18 +0300

    OD: Notes and OperationResult enhancements

--- Src/H.Necessaire/H.Necessaire/Models/Note.cs
+++ Src/H.Necessaire/H.Necessaire/Models/Note.cs
@@ -25,14 +25,6 @@ namespace H.Necessaire
             return $"{ID} {IDSeparator} {Value}";
         }
 
-        public static Note[] FromDictionary(IDictionary<string, string> keyValuePairs)
-        {
-            if (!keyValuePairs?.Any() ?? true)
-                return new Note[0];
-
-            return keyValuePairs.Select(x => new Note(x.Key, x.Value)).ToArray();
-        }
-
         public static implicit operator Note(string noteAsString)
         {
             if (noteAsString.IsEmpty())
@@ -49,6 +41,46 @@ namespace H.Necessaire
                 return null;
 
             return note.ToString();
+        }
+
+        public static Note[] FromDictionary(IDictionary<string, string> keyValuePairs)
+        {
+            if (!keyValuePairs?.Any() ?? true)
+                return new Note[0];
+
+            return keyValuePairs.Select(x => new Note(x.Key, x.Value)).ToArray();
+        }
+
+        public static Note[] GetEnvironmentInfo()
+        {
+            return
+                new Note[] {
+
+                    $"{nameof(Environment.MachineName)}{IDSeparator}{Environment.MachineName}",
+                    $"{nameof(Environment.ProcessorCount)}{IDSeparator}{Environment.ProcessorCount}",
+                    $"{nameof(Environment.UserDomainName)}{IDSeparator}{Environment.UserDomainName}",
+                    $"{nameof(Environment.UserName)}{IDSeparator}{Environment.UserName}",
+                    $"{nameof(Environment.UserInteractive)}{IDSeparator}{Environment.UserInteractive}",
+
+                    $"{nameof(Environment.OSVersion)}{IDSeparator}{Environment.OSVersion}",
+
+                    $"{nameof(Environment.Is64BitOperatingSystem)}{IDSeparator}{Environment.Is64BitOperatingSystem}",
+                    $"{nameof(Environment.Is64BitProcess)}{IDSeparator}{Environment.Is64BitProcess}",
+
+                    $"{nameof(Environment.Version)}{IDSeparator}{Environment.Version}",
+
+                    $"{nameof(Environment.CommandLine)}{IDSeparator}{Environment.CommandLine}",
+                    $"{nameof(Environment.CurrentDirectory)}{IDSeparator}{Environment.CurrentDirectory}",
+                    $"{nameof(Environment.SystemDirectory)}{IDSeparator}{Environment.SystemDirectory}",
+                    $"{nameof(Environment.NewLine)}{IDSeparator}{Environment.NewLine}",
+
+                    $"{nameof(Environment.SystemPageSize)}{IDSeparator}{Environment.SystemPageSize}",
+                    $"{nameof(Environment.TickCount)}{IDSeparator}{Environment.TickCount}",
+                    $"{nameof(Environment.WorkingSet)}{IDSeparator}{Environment.WorkingSet}",
+
+                    $"{nameof(Environment.CurrentManagedThreadId)}{IDSeparator}{Environment.CurrentManagedThreadId}",
+
+                };
         }
     }
 }
