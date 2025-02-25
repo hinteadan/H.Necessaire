9c483c53b1934b3fb624fbc849524164e41b6388
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2025-02-25 14:52:51 +0200

    OD: HUI Components

--- Src/H.Necessaire/H.Necessaire.Runtime.MAUI/Components/Controls/HDatePicker.cs
+++ Src/H.Necessaire/H.Necessaire.Runtime.MAUI/Components/Controls/HDatePicker.cs
@@ -12,7 +12,9 @@ namespace H.Necessaire.Runtime.MAUI.Components.Controls
                 FontFamily = HUiToolkit.Current.Branding.Typography.FontFamily,
                 FontSize = HUiToolkit.Current.Branding.Typography.FontSize,
                 TextColor = HUiToolkit.Current.Branding.TextColor.ToMaui(),
-            };
+                Format = "yyyy/MM/dd",
+                HorizontalOptions = LayoutOptions.End,
+            }.Bordered();
         }
     }
 }
