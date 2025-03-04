4c3a6278503dd732678416d6e158fac685114478
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2025-03-04 09:44:59 +0200

    OD: HUI Components

--- Src/H.Necessaire/H.Necessaire.Runtime.MAUI/Components/Controls/HPicker.cs
+++ Src/H.Necessaire/H.Necessaire.Runtime.MAUI/Components/Controls/HPicker.cs
@@ -7,7 +7,7 @@ namespace H.Necessaire.Runtime.MAUI.Components.Controls
     public class HPicker : HMauiLabelAndDescriptionComponentBase
     {
         Picker editor;
-
+        public event EventHandler SelectedIndexChanged;
         protected override View ConstructLabeledContent()
         {
             return
@@ -30,6 +30,18 @@ namespace H.Necessaire.Runtime.MAUI.Components.Controls
         public IList ItemSource { get => editor.ItemsSource; set => editor.ItemsSource = value; }
         public IList<string> Items => editor.Items;
 
+        BindingBase itemDisplayBinding = null;
+        public BindingBase ItemDisplayBinding
+        {
+            get => itemDisplayBinding;
+            set
+            {
+                itemDisplayBinding = value;
+                if (editor is not null)
+                    editor.ItemDisplayBinding = itemDisplayBinding;
+            }
+        }
+
         View ConstructEditor()
         {
             return new Picker
@@ -39,6 +51,7 @@ namespace H.Necessaire.Runtime.MAUI.Components.Controls
                 TextColor = Branding.TextColor.ToMaui(),
                 BackgroundColor = Branding.BackgroundColorTranslucent.ToMaui(),
                 VerticalTextAlignment = TextAlignment.Center,
+                ItemDisplayBinding = itemDisplayBinding,
             }
             .And(x => x.SelectedIndexChanged += OnPickerSelectedIndexChanged)
             .And(x => editor = x)
@@ -47,7 +60,7 @@ namespace H.Necessaire.Runtime.MAUI.Components.Controls
 
         void OnPickerSelectedIndexChanged(object sender, EventArgs e)
         {
-
+            SelectedIndexChanged?.Invoke(sender, e);
         }
     }
 }
---
+++ Src/H.Necessaire/H.Necessaire.Runtime.MAUI/Components/Controls/HThemeSelector.cs
@@ -0,0 +1,49 @@
+﻿using H.Necessaire.Runtime.MAUI.Components.Abstracts;
+
+namespace H.Necessaire.Runtime.MAUI.Components.Controls
+{
+    public class HThemeSelector : HMauiComponentBase
+    {
+        static readonly KeyValuePair<string, AppTheme>[] options = new KeyValuePair<string, AppTheme>[] {
+            new KeyValuePair<string, AppTheme>("System", AppTheme.Unspecified),
+            new KeyValuePair<string, AppTheme>("Light", AppTheme.Light),
+            new KeyValuePair<string, AppTheme>("Dark", AppTheme.Dark),
+        };
+
+        protected override View ConstructContent()
+        {
+            AppTheme currentTheme = GetCurrentTheme();
+
+            return new Grid { }.And(layout =>
+            {
+
+                layout.Add(new HPicker
+                {
+                    ItemSource = options,
+                    ItemDisplayBinding = Binding.Create(static (KeyValuePair<string, AppTheme> entry) => entry.Key),
+                    SelectedIndex = Array.IndexOf(options, options.Single(x => x.Value == currentTheme)),
+
+                }.And(picker =>
+                {
+                    picker.SelectedIndexChanged += (s, e) =>
+                    {
+                        AppTheme selectedTheme = ((KeyValuePair<string, AppTheme>)picker.SelectedItem).Value;
+
+                        Application.Current.UserAppTheme = selectedTheme;
+
+                    };
+                }));
+
+            });
+        }
+
+        static AppTheme GetCurrentTheme()
+        {
+            AppTheme? theme = Application.Current?.UserAppTheme;
+            if (theme is not null && theme != AppTheme.Unspecified)
+                return theme.Value;
+
+            return Application.Current?.RequestedTheme ?? AppTheme.Unspecified;
+        }
+    }
+}
