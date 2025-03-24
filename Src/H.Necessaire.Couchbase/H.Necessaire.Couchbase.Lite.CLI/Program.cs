0cce282d2faff923512c27a4cadc4b69c7fdf2e5
Dan Hintea Qubiz <dan.hintea@qubiz.com>
2025-03-24 15:57:00 +0200

    Scaffold Couchbase solution

---
+++ Src/H.Necessaire.Couchbase/H.Necessaire.Couchbase.Lite.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace H.Necessaire.Couchbase.Lite.CLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
