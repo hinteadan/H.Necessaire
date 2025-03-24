64c23ac4efd803186359e045a70e5c29cf8fc085
Dan Hintea Qubiz <dan.hintea@qubiz.com>
2025-03-24 16:06:56 +0200

    OD: Scaffold Couchbase solution

---
+++ Src/H.Necessaire.Couchbase/H.Necessaire.Couchbase.Lite.CLI/CouchbaseCommand.cs
@@ -0,0 +1,14 @@
+﻿using H.Necessaire.CLI.Commands;
+using System.Threading.Tasks;
+
+namespace H.Necessaire.Couchbase.Lite.CLI
+{
+    [Alias("couch")]
+    internal class CouchbaseCommand : CommandBase
+    {
+        public override Task<OperationResult> Run()
+        {
+            return OperationResult.Fail("Not yet implemented").AsTask();
+        }
+    }
+}
---
+++ Src/H.Necessaire.Couchbase/H.Necessaire.Couchbase.Lite/🏭IoCExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace H.Necessaire.Couchbase.Lite
+{
+    public static class IoCExtensions
+    {
+        public static T WithHNecessaireCouchbaseLite<T>(this T depsRegistry) where T : ImADependencyRegistry
+        {
+            return depsRegistry;
+        }
+    }
+}
