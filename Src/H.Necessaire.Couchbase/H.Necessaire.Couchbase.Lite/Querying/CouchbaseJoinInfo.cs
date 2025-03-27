001f4f8d897926cbefab6b2077b7dc1bf4a1ca83
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2025-03-27 18:06:07 +0200

    OD: Couchbase JOIN API

--- Src/H.Necessaire.Couchbase/H.Necessaire.Couchbase.Lite/Querying/CouchbaseJoinInfo.cs
+++ Src/H.Necessaire.Couchbase/H.Necessaire.Couchbase.Lite/Querying/CouchbaseJoinInfo.cs
@@ -1,9 +1,30 @@
-﻿namespace H.Necessaire.Couchbase.Lite.Querying
+﻿using System;
+using System.Linq.Expressions;
+
+namespace H.Necessaire.Couchbase.Lite.Querying
 {
-    public class CouchbaseJoinInfo
+    public class CouchbaseJoinInfo<TThis, TThat>
     {
-        public CouchbaseJoinType JoinType { get; set; } = CouchbaseJoinType.LeftOuter;
-        public string CollectionName { get; set; }
-        public string CollectionAlias { get; set; }
+        public CouchbaseJoinInfo(string collectionName, string collectionAlias, Expression<Func<TThis, TThat, bool>> joinBy, CouchbaseJoinType joinType = CouchbaseJoinType.LeftOuter)
+        {
+            JoinType = joinType;
+            CollectionName = collectionName;
+            CollectionAlias = collectionAlias;
+            JoinBy = joinBy;
+        }
+
+        public CouchbaseJoinInfo(string collectionName, Expression<Func<TThis, TThat, bool>> joinBy, CouchbaseJoinType joinType = CouchbaseJoinType.LeftOuter)
+            : this(collectionName, collectionName, joinBy, joinType) { }
+
+        public CouchbaseJoinInfo(Expression<Func<TThis, TThat, bool>> joinBy, CouchbaseJoinType joinType = CouchbaseJoinType.LeftOuter)
+            : this(typeof(TThat).Name, joinBy, joinType) { }
+
+        public CouchbaseJoinType JoinType { get; } = CouchbaseJoinType.LeftOuter;
+        public string CollectionName { get; }
+        public string CollectionAlias { get; }
+        public Expression<Func<TThis, TThat, bool>> JoinBy { get; }
+
+        public static implicit operator CouchbaseJoinInfo<TThis, TThat>(Expression<Func<TThis, TThat, bool>> joinBy)
+            => new CouchbaseJoinInfo<TThis, TThat>(joinBy);
     }
 }
