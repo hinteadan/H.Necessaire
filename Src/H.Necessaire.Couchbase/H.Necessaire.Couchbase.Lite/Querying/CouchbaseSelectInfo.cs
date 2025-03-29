29ba37a2cc096fa0f8b5fce524b14f0eccb396ee
Hintee <hintea_dan@yahoo.co.uk>
2025-03-29 07:03:58 +0100

    OD: Couchbase JOIN API

--- Src/H.Necessaire.Couchbase/H.Necessaire.Couchbase.Lite/Querying/CouchbaseSelectInfo.cs
+++ Src/H.Necessaire.Couchbase/H.Necessaire.Couchbase.Lite/Querying/CouchbaseSelectInfo.cs
@@ -1,6 +1,26 @@
-﻿namespace H.Necessaire.Couchbase.Lite.Querying
+﻿using System;
+using System.Linq.Expressions;
+
+namespace H.Necessaire.Couchbase.Lite.Querying
 {
-    public class CouchbaseSelectInfo
+    public class CouchbaseSelectInfo<T>
     {
+        private CouchbaseSelectInfo(Expression<Func<T, object>> selector, string fromAlias, string nameAlias)
+        {
+            Selector = selector;
+            FromAlias = fromAlias;
+            NameAlias = nameAlias;
+        }
+
+        public Expression<Func<T, object>> Selector { get; }
+        public string FromAlias { get; }
+        public string NameAlias { get; }
+
+        public static implicit operator CouchbaseSelectInfo<T>((Expression<Func<T, object>> selector, string fromAlias, string nameAlias) select)
+            => new CouchbaseSelectInfo<T>(select.selector, select.fromAlias, select.nameAlias);
+        public static implicit operator CouchbaseSelectInfo<T>((Expression<Func<T, object>> selector, string fromAlias) select)
+            => new CouchbaseSelectInfo<T>(select.selector, select.fromAlias, null);
+        public static implicit operator CouchbaseSelectInfo<T>(Expression<Func<T, object>> select)
+            => new CouchbaseSelectInfo<T>(select, null, null);
     }
 }
