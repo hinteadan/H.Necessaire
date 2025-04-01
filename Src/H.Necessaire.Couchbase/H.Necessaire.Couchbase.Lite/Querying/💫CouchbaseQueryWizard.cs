3092d4c59afabd17076ec124bd753f793000c45c
Hintee <hintea_dan@yahoo.co.uk>
2025-04-01 13:33:57 +0200

    OD: Couchbase JOIN API

--- Src/H.Necessaire.Couchbase/H.Necessaire.Couchbase.Lite/Querying/💫CouchbaseQueryWizard.cs
+++ Src/H.Necessaire.Couchbase/H.Necessaire.Couchbase.Lite/Querying/💫CouchbaseQueryWizard.cs
@@ -26,10 +26,22 @@ namespace H.Necessaire.Couchbase.Lite.Querying
                 new SelectOperationResult<T>(DataSource.Collection(operationScope.Collection), CouchbaseLinqExpressionInterpreter.Instance.Select(selectors));
         }
 
+        public static SelectOperationResult<T> Select<T>(this CouchbaseOperationScope operationScope, params ISelectResult[] selects)
+        {
+            return
+                new SelectOperationResult<T>(DataSource.Collection(operationScope.Collection), selects);
+        }
+
         public static SelectOperationResult<T> Select<T>(this CouchbaseOperationScope operationScope, string alias, params Expression<Func<T, object>>[] selectors)
         {
             return
                 new SelectOperationResult<T>(DataSource.Collection(operationScope.Collection).As(alias), CouchbaseLinqExpressionInterpreter.Instance.Select(alias, selectors));
+        }
+
+        public static SelectOperationResult<T> Select<T>(this CouchbaseOperationScope operationScope, string alias, params ISelectResult[] selects)
+        {
+            return
+                new SelectOperationResult<T>(DataSource.Collection(operationScope.Collection).As(alias), selects);
         }
     }
 
