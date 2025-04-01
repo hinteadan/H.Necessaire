78132aff9ecff5fb61c1e06873223f7570c925eb
Hintee <hintea_dan@yahoo.co.uk>
2025-04-01 18:47:53 +0200

    OD: Couchbase JOIN API

---
+++ Src/H.Necessaire.Couchbase/H.Necessaire.Couchbase.Lite/HCb.cs
@@ -0,0 +1,14 @@
+﻿using Couchbase.Lite.Query;
+using H.Necessaire.Couchbase.Lite.Querying;
+
+namespace H.Necessaire.Couchbase.Lite
+{
+    public static class HCb
+    {
+        static readonly CouchbaseLinqExpressionInterpreter interpreter = CouchbaseLinqExpressionInterpreter.Instance;
+
+        public static ISelectResult Select<T>(this CouchbaseSelectInfo<T> selectInfo) => interpreter.Select(selectInfo);
+
+        public static IJoin Join<TThis, TThat>(this CouchbaseJoinInfo<TThis, TThat> joinInfo) => interpreter.Join(joinInfo);
+    }
+}
