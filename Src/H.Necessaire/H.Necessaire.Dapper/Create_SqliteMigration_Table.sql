dcadfe69338a292aa3a7bb1bf096980a30be2f93
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2025-04-10 23:57:11 +0300

    OD: Dapper refactoring

--- Src/H.Necessaire/H.Necessaire.Dapper/Create_SqliteMigration_Table.sql
+++ Src/H.Necessaire/H.Necessaire.Dapper/Create_SqliteMigration_Table.sql
@@ -1,48 +1,34 @@
-﻿CREATE TABLE [dbo].[H.Necessaire.Migration] 
+﻿CREATE TABLE "H.Necessaire.Migration" (
+	"ID"	TEXT NOT NULL,
+	"ResourceIdentifier"	TEXT NOT NULL,
+	"VersionNumber"	TEXT NOT NULL,
+	"HappenedAt"	TEXT NOT NULL,
+	"HappenedAtTicks"	INTEGER NOT NULL,
+	"SqlCommand"	TEXT NOT NULL,
+	PRIMARY KEY("ID")
+);
+
+
+
+CREATE INDEX "IX_H.Necessaire.Migration_ResourceIdentifier" ON "H.Necessaire.Migration"
 (
-	[ID] [nvarchar](450) NOT NULL,
-	[ResourceIdentifier] [nvarchar](450) NOT NULL,
-	[VersionNumber] [nvarchar](450) NOT NULL,
-	[HappenedAt] [datetime2](7) NOT NULL,
-	[HappenedAtTicks] [bigint] NOT NULL,
-	[SqlCommand] [ntext] NOT NULL,
-
-	CONSTRAINT [PK_H.Necessaire.Migration] PRIMARY KEY
-	(
-		[ID] ASC
-	) 
-	WITH 
-	(
-		STATISTICS_NORECOMPUTE = OFF,
-		IGNORE_DUP_KEY = OFF,
-		OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF
-	)
-	ON [PRIMARY]
-
-)
-
-ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+	"ResourceIdentifier"
+);
 
 
-CREATE NONCLUSTERED INDEX [IX_H.Necessaire.Migration_ResourceIdentifier] ON [dbo].[H.Necessaire.Migration]
+CREATE INDEX "IX_H.Necessaire.Migration_VersionNumber" ON "H.Necessaire.Migration"
 (
-	[ResourceIdentifier] ASC
-)WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+	"VersionNumber"
+);
 
 
-CREATE NONCLUSTERED INDEX [IX_H.Necessaire.Migration_VersionNumber] ON [dbo].[H.Necessaire.Migration]
+CREATE INDEX "IX_H.Necessaire.Migration_HappenedAt" ON "H.Necessaire.Migration"
 (
-	[VersionNumber] ASC
-)WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+	"HappenedAt"
+);
 
 
-CREATE NONCLUSTERED INDEX [IX_H.Necessaire.Migration_HappenedAt] ON [dbo].[H.Necessaire.Migration]
+CREATE INDEX "IX_H.Necessaire.Migration_HappenedAtTicks" ON "H.Necessaire.Migration"
 (
-	[HappenedAt] ASC
-)WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-
-
-CREATE NONCLUSTERED INDEX [IX_H.Necessaire.Migration_HappenedAtTicks] ON [dbo].[H.Necessaire.Migration]
-(
-	[HappenedAtTicks] ASC
-)WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+	"HappenedAtTicks"
+);
