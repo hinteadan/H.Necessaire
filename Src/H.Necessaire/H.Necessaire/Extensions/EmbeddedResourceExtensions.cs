1a15dd0c415f9a20206412b4bf210ea9e88f7af6
Dan Hintea Qubiz <dan.hintea@qubiz.com>
2025-04-11 11:31:24 +0300

    OD: Dapper refactoring

--- Src/H.Necessaire/H.Necessaire/Extensions/EmbeddedResourceExtensions.cs
+++ Src/H.Necessaire/H.Necessaire/Extensions/EmbeddedResourceExtensions.cs
@@ -7,28 +7,31 @@ namespace H.Necessaire
 {
     public static class EmbeddedResourceExtensions
     {
-        public static Stream OpenEmbeddedResource(this string embeddedResourceName)
+        public static Stream OpenEmbeddedResource(this string embeddedResourceName, Assembly embeddedResourceAssembly = null)
         {
-            Assembly embeddedResourceAssembly = null;
             string embeddedResourceFullName = null;
 
-            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+            if (embeddedResourceAssembly is null)
             {
-                new Action(() =>
-                    {
-                        embeddedResourceFullName = assembly.GetManifestResourceNames().FirstOrDefault(x => x.EndsWith(embeddedResourceName, StringComparison.InvariantCultureIgnoreCase));
-                    }
-                ).TryOrFailWithGrace();
 
-                if (embeddedResourceFullName != null)
+                foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
                 {
-                    embeddedResourceAssembly = assembly;
-                    break;
-                }
-            }
+                    new Action(() =>
+                        {
+                            embeddedResourceFullName = assembly.GetManifestResourceNames().FirstOrDefault(x => x.EndsWith(embeddedResourceName, StringComparison.InvariantCultureIgnoreCase));
+                        }
+                    ).TryOrFailWithGrace();
 
-            if (embeddedResourceAssembly == null)
-                return null;
+                    if (embeddedResourceFullName != null)
+                    {
+                        embeddedResourceAssembly = assembly;
+                        break;
+                    }
+                }
+
+                if (embeddedResourceAssembly == null)
+                    return null;
+            }
 
             return embeddedResourceAssembly.GetManifestResourceStream(embeddedResourceFullName);
         }
