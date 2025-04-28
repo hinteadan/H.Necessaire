d8e331252bdac0c01f7c1e36b5de7e3929dc337d
Dan Hintea Qubiz <dan.hintea@qubiz.com>
2025-04-28 15:32:46 +0300

    OD: Core building blocks

--- Src/H.Necessaire/H.Necessaire.Runtime.Sqlite/Core/Resources/LogEntrySqliteRsx.cs
+++ Src/H.Necessaire/H.Necessaire.Runtime.Sqlite/Core/Resources/LogEntrySqliteRsx.cs
@@ -4,7 +4,6 @@ using H.Necessaire.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace H.Necessaire.Runtime.Sqlite.Core.Resources
