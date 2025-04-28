4b9b756c138a6bad917ba9563a34627ce866a499
Dan Hintea Qubiz <dan.hintea@qubiz.com>
2025-04-28 14:31:49 +0300

    OD: Core building blocks

--- Src/H.Necessaire/H.Necessaire.Runtime.Sqlite/Core/Resources/DependencyGroup.cs
+++ Src/H.Necessaire/H.Necessaire.Runtime.Sqlite/Core/Resources/DependencyGroup.cs
@@ -18,6 +18,20 @@ namespace H.Necessaire.Runtime.Sqlite.Core.Resources
                 .Register<ImAStorageBrowserService<LogEntry, LogFilter>>(() => dependencyRegistry.Get<LogEntrySqliteRsx>())
                 ;
 
+            dependencyRegistry
+                .Register<LogEntrySqliteRsx>(() => new LogEntrySqliteRsx())
+                .Register<ImAStorageService<Guid, LogEntry>>(() => dependencyRegistry.Get<LogEntrySqliteRsx>())
+                .Register<ImAStorageBrowserService<LogEntry, LogFilter>>(() => dependencyRegistry.Get<LogEntrySqliteRsx>())
+                ;
+
+            dependencyRegistry
+               .Register<DataBinSqliteRsx>(() => new DataBinSqliteRsx())
+               .Register<ImAStorageService<Guid, DataBinMeta>>(() => dependencyRegistry.Get<DataBinSqliteRsx>())
+               .Register<ImAStorageBrowserService<DataBinMeta, DataBinFilter>>(() => dependencyRegistry.Get<DataBinSqliteRsx>())
+               .Register<ImAStorageService<Guid, DataBin>>(() => dependencyRegistry.Get<DataBinSqliteRsx>())
+               .Register<ImAStorageBrowserService<DataBin, DataBinFilter>>(() => dependencyRegistry.Get<DataBinSqliteRsx>())
+               ;
+
             dependencyRegistry.Register<Auditing.DependencyGroup>(() => new Auditing.DependencyGroup());
         }
     }
