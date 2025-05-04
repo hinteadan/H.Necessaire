f523e808096e92ceeb5e7b362f9cc4d251f77c7f
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2025-05-04 09:39:46 +0300

    OD: MAUI Versioning

--- Src/H.Necessaire/H.Necessaire/Operations/Versioning/Concrete/EmbeddedResourceVersionProvider.cs
+++ Src/H.Necessaire/H.Necessaire/Operations/Versioning/Concrete/EmbeddedResourceVersionProvider.cs
@@ -1,44 +1,9 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
+﻿using H.Necessaire.Operations.Versioning.Abstract;
 
 namespace H.Necessaire.Operations.Versioning.Concrete
 {
-    internal class EmbeddedResourceVersionProvider : ImAVersionProvider
+    internal class EmbeddedResourceVersionProvider : EmbeddedResourceVersionProviderBase
     {
-        static readonly Version defaultVersion = Version.Unknown;
 
-        static Version currentVersion = null;
-
-        public async Task<Version> GetCurrentVersion()
-        {
-            if (currentVersion != null)
-                return currentVersion;
-
-            Assembly entryAssembly = Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly();
-
-            string versionResourceName = entryAssembly.GetManifestResourceNames()?.FirstOrDefault(x => x.EndsWith("version.txt", StringComparison.InvariantCultureIgnoreCase));
-
-            if (string.IsNullOrWhiteSpace(versionResourceName))
-            {
-                currentVersion = defaultVersion;
-                return currentVersion;
-            }
-
-            using (Stream stream = entryAssembly.GetManifestResourceStream(versionResourceName))
-            {
-                await
-                    new Func<Task>(async () =>
-                        currentVersion = Version.Parse(
-                            await stream.ReadAsStringAsync()
-                        )
-                    )
-                    .TryOrFailWithGrace(onFail: ex => currentVersion = defaultVersion);
-            }
-
-            return currentVersion;
-        }
     }
 }
