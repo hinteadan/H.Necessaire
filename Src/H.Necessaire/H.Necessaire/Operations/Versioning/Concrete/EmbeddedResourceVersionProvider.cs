3b80010840be37dbff3869531607b336edcdbde8
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2025-05-04 09:58:10 +0300

    OD: MAUI Versioning

--- Src/H.Necessaire/H.Necessaire/Operations/Versioning/Concrete/EmbeddedResourceVersionProvider.cs
+++ Src/H.Necessaire/H.Necessaire/Operations/Versioning/Concrete/EmbeddedResourceVersionProvider.cs
@@ -1,9 +1,14 @@
 ﻿using H.Necessaire.Operations.Versioning.Abstract;
+using System.Reflection;
 
 namespace H.Necessaire.Operations.Versioning.Concrete
 {
     internal class EmbeddedResourceVersionProvider : EmbeddedResourceVersionProviderBase
     {
         public EmbeddedResourceVersionProvider() : base() { }
+
+        public EmbeddedResourceVersionProvider(params Assembly[] assembliesToScan) : base(assembliesToScan) { }
+
+        public EmbeddedResourceVersionProvider(string versionFileName, params Assembly[] assembliesToScan) : base(versionFileName, assembliesToScan) { }
     }
 }
