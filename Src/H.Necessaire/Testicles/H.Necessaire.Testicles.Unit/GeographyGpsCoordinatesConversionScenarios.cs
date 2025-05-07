b6052acb3378aec9b919ebed16f5f2734cac8368
Dan Hintea Qubiz <dan.hintea@qubiz.com>
2025-05-07 09:56:18 +0300

    OD: Airports UI

---
+++ Src/H.Necessaire/Testicles/H.Necessaire.Testicles.Unit/GeographyGpsCoordinatesConversionScenarios.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace H.Necessaire.Testicles.Unit
+{
+    public class GeographyGpsCoordinatesConversionScenarios
+    {
+        [Fact(DisplayName = "GeographyGpsCoordinates are correctly being converted from Decimal to DMS")]
+        public void GeographyGpsCoordinates_Decimal_To_DMS()
+        {
+
+        }
+
+        [Fact(DisplayName = "GeographyGpsCoordinates are correctly being converted from DMS to Decimal")]
+        public void GeographyGpsCoordinate_DMS_To_Decimal()
+        {
+
+        }
+    }
+}
