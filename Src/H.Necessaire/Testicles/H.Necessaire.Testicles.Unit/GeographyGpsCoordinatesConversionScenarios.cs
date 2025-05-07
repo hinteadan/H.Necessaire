bda9cf9b8fa2581b46e8a1d292fec679fcbea910
Dan Hintea Qubiz <dan.hintea@qubiz.com>
2025-05-07 11:43:35 +0300

    OD: Airports UI

--- Src/H.Necessaire/Testicles/H.Necessaire.Testicles.Unit/GeographyGpsCoordinatesConversionScenarios.cs
+++ Src/H.Necessaire/Testicles/H.Necessaire.Testicles.Unit/GeographyGpsCoordinatesConversionScenarios.cs
@@ -22,22 +22,22 @@ namespace H.Necessaire.Testicles.Unit
             GpsPoint gpsPoint = (46.459110, 23.552939);
             GeoDmsCoordinates dmsPoint = gpsPoint;
             GeoDmsCoordinates expectedDmsPoint = ((46, 27, 32.8, GeoDmsLatDirection.North), (23, 33, 10.6, GeoDmsLngDirection.East));
-            dmsPoint.Should().Be(expectedDmsPoint, because: "that's the conversion Google Maps gives for NE hemisphere");
+            AssertDmsCoordinates(dmsPoint, expectedDmsPoint, because: "that's the conversion Google Maps gives for NE hemisphere");
 
             gpsPoint = (47.686120, -112.690994);
             dmsPoint = gpsPoint;
             expectedDmsPoint = ((47, 41, 10.0, GeoDmsLatDirection.North), (112, 41, 27.6, GeoDmsLngDirection.West));
-            dmsPoint.Should().Be(expectedDmsPoint, because: "that's the conversion Google Maps gives for NW hemisphere");
+            AssertDmsCoordinates(dmsPoint, expectedDmsPoint, because: "that's the conversion Google Maps gives for NW hemisphere");
 
             gpsPoint = (-33.168682, -67.149395);
             dmsPoint = gpsPoint;
             expectedDmsPoint = ((33, 10, 7.3, GeoDmsLatDirection.South), (67, 8, 57.8, GeoDmsLngDirection.West));
-            dmsPoint.Should().Be(expectedDmsPoint, because: "that's the conversion Google Maps gives for SW hemisphere");
+            AssertDmsCoordinates(dmsPoint, expectedDmsPoint, because: "that's the conversion Google Maps gives for SW hemisphere");
 
             gpsPoint = (-33.918148, 141.268403);
             dmsPoint = gpsPoint;
             expectedDmsPoint = ((33, 55, 5.3, GeoDmsLatDirection.South), (141, 16, 6.3, GeoDmsLngDirection.East));
-            dmsPoint.Should().Be(expectedDmsPoint, because: "that's the conversion Google Maps gives for SE hemisphere");
+            AssertDmsCoordinates(dmsPoint, expectedDmsPoint, because: "that's the conversion Google Maps gives for SE hemisphere");
         }
 
         [Fact(DisplayName = "GeographyGpsCoordinates are correctly being converted from DMS to Decimal")]
@@ -49,7 +49,7 @@ namespace H.Necessaire.Testicles.Unit
         [Fact(DisplayName = "ToDMS(this double degrees, out int deg, out int min, out double sec) works as expected")]
         public void DegreesToDMS_Extension_Method_Works_as_Expected()
         {
-            0.000000.ToDMS(out var deg, out var min, out var sec); 
+            0.000000.ToDMS(out var deg, out var min, out var sec);
             deg.Should().Be(0, because: "0.000000° has 0°");
             min.Should().Be(0, because: "0.000000 has 0'");
             sec.Should().Be(0, because: "0.000000 has 0\"");
@@ -77,6 +77,19 @@ namespace H.Necessaire.Testicles.Unit
             45.ToDegrees(0, 0.0).Should().BeApproximately(45.000000, 6, because: "45°0'0\" is 45.000000°");
             15.ToDegrees(7, 30.0000).Should().BeApproximately(15.125000, 6, because: "15°07'30.00\" is 15.125000°");
             46.ToDegrees(27, 32.8000).Should().BeApproximately(46.459110, 6, because: "46°27'32.80\" is 46.459110°");
+        }
+
+        static void AssertDmsCoordinates(GeoDmsCoordinates dmsPoint, GeoDmsCoordinates expectedDmsPoint, string because = null)
+        {
+            dmsPoint.Lat.Degrees.Should().Be(expectedDmsPoint.Lat.Degrees, because);
+            dmsPoint.Lat.Minutes.Should().Be(expectedDmsPoint.Lat.Minutes, because);
+            dmsPoint.Lat.Seconds.Should().BeApproximately(expectedDmsPoint.Lat.Seconds, 1, because);
+            dmsPoint.Lat.Direction.Should().Be(expectedDmsPoint.Lat.Direction, because);
+
+            dmsPoint.Lng.Degrees.Should().Be(expectedDmsPoint.Lng.Degrees, because);
+            dmsPoint.Lng.Minutes.Should().Be(expectedDmsPoint.Lng.Minutes, because);
+            dmsPoint.Lng.Seconds.Should().BeApproximately(expectedDmsPoint.Lng.Seconds, 1, because);
+            dmsPoint.Lng.Direction.Should().Be(expectedDmsPoint.Lng.Direction, because);
         }
     }
 }
