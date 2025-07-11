d55782ea0a6f2b037d703d2ee09b547b36845079
Hintee <hintea_dan@yahoo.co.uk>
2025-07-11 21:18:44 +0300

    ConnectivityManager

--- Src/H.Necessaire/H.Necessaire/Operations/ImAHealthChecker.cs
+++ Src/H.Necessaire/H.Necessaire/Operations/ImAHealthChecker.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace H.Necessaire
 {
@@ -20,11 +21,11 @@ namespace H.Necessaire
         static readonly TimeSpan connectivityCheckTimeout = TimeSpan.FromSeconds(3);
         static readonly TimeSpan httpClientTimeout = TimeSpan.FromMinutes(5);
         static EphemeralType<HttpClient> ephemeralHttpClient = null;
-        static EphemeralType<OperationResult> defaultConnectivityCheckResult = null;
         const string defaultUrlToCheckInternet = "http://www.msftncsi.com/ncsi.txt";
 
         readonly ConcurrentDictionary<string, Func<Task<OperationResult>>> connectivityChecks = new ConcurrentDictionary<string, Func<Task<OperationResult>>>();
         readonly ConcurrentDictionary<string, EphemeralType<OperationResult>> connectivityCheckResults = new ConcurrentDictionary<string, EphemeralType<OperationResult>>();
+        readonly ConcurrentDictionary<string, EphemeralType<OperationResult>> httpConnectivityCheckResults = new ConcurrentDictionary<string, EphemeralType<OperationResult>>();
 
         public bool IsInternetConnectionCheckDisabled { get; set; } = false;
         bool IsInternetConnectionCheckEnabled => !IsInternetConnectionCheckDisabled;
@@ -51,7 +52,7 @@ namespace H.Necessaire
                 return defaultCheck.WithPayload(defaultCheck.Tag("DefaultCheck").Describe($"HTTP request to {defaultUrlToCheckInternet}").AsArray());
             }
 
-            TaggedValue<OperationResult>[] checkResults = await Task.WhenAll(connectivityChecks.Select(kvp => RunConnectivityCheck(kvp.Key, kvp.Value)));
+            TaggedValue<OperationResult>[] checkResults = await Task.WhenAll(connectivityChecks.Select(kvp => RunHealthCheck(kvp.Key, kvp.Value)));
 
             OperationResult globalResult = checkResults.Select(x => x.Value).Merge("Connectivity is unavailable for some of the checks. See comments for details");
 
@@ -62,7 +63,7 @@ namespace H.Necessaire
 
         protected virtual Task<bool> HasSurelyNoInternet() => false.AsTask();
 
-        async Task<TaggedValue<OperationResult>> RunConnectivityCheck(string name, Func<Task<OperationResult>> connectivityCheck)
+        async Task<TaggedValue<OperationResult>> RunHealthCheck(string name, Func<Task<OperationResult>> connectivityCheck)
         {
             if (IsInternetConnectionCheckEnabled && await HasSurelyNoInternet())
                 return OperationResult.Fail("No Internet Connection").Tag(name);
@@ -86,36 +87,43 @@ namespace H.Necessaire
             return checkResult.Payload.Tag(name);
         }
 
-        async Task<OperationResult> CheckDefaultConnectivity()
+        async Task<OperationResult> RunHttpRequestHealthCheck(string url)
         {
             if (await HasSurelyNoInternet())
                 return "No Internet Connection";
 
-            if (defaultConnectivityCheckResult?.IsActive() == true)
-                return defaultConnectivityCheckResult.Payload;
+            if (httpConnectivityCheckResults.TryGetValue(url, out var check) && check?.IsActive() == true)
+                return check.Payload;
 
             var result = await HSafe.Run<OperationResult>(async () =>
             {
                 var http = EnsureHttpClient();
 
-                using (var httpResponse = await http.GetAsync(defaultUrlToCheckInternet, HttpCompletionOption.ResponseHeadersRead))
+                using (var httpResponse = await http.GetAsync(url, HttpCompletionOption.ResponseHeadersRead))
                 {
                     if (!httpResponse.IsSuccessStatusCode)
                     {
-                        return $"Error occurred while trying to ping {defaultUrlToCheckInternet}. HTTP Response Code: {(int)httpResponse.StatusCode} - {httpResponse.StatusCode}";
+                        return $"Error occurred while trying to ping {url}. HTTP Response Code: {(int)httpResponse.StatusCode} - {httpResponse.StatusCode}";
                     }
                 }
 
                 return true;
             });
 
-            defaultConnectivityCheckResult = new EphemeralType<OperationResult>
+            var checkResult = new EphemeralType<OperationResult>
             {
                 Payload = !result ? result as OperationResult : result.Payload,
                 ValidFor = connectivityCheckTimeout,
             };
 
-            return defaultConnectivityCheckResult.Payload;
+            httpConnectivityCheckResults.AddOrUpdate(url, checkResult, (key, existing) => checkResult);
+
+            return checkResult.Payload;
+        }
+
+        async Task<OperationResult> CheckDefaultConnectivity()
+        {
+            return await RunHttpRequestHealthCheck(defaultUrlToCheckInternet);
         }
 
         static HttpClient EnsureHttpClient()
