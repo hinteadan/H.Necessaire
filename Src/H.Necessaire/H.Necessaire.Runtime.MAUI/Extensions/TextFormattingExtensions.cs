3c1aa5f8e2e5563cefbe21044c5d6ab133509658
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2025-07-17 21:07:28 +0300

    OD: Add legal docs in app

---
+++ Src/H.Necessaire/H.Necessaire.Runtime.MAUI/Extensions/TextFormattingExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace H.Necessaire.Runtime.MAUI.Extensions
+{
+    public static class TextFormattingExtensions
+    {
+        public static FormattedString Add(this FormattedString formattedString, string value, Action<Span> formatter = null)
+        {
+            if (value.IsEmpty())
+                return formattedString;
+
+            if (formattedString is null)
+                return value;
+
+            formattedString.Spans.Add(new Span
+            {
+                Text = value,
+            }.AndIf(formatter is not null, formatter));
+
+            return formattedString;
+        }
+    }
+}
