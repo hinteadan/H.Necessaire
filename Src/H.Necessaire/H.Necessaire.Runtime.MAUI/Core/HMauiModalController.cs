7a08eac3c86d1d1d6f9afda5d3a9e37a5e57f29e
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2025-08-06 16:21:12 +0300

    OD: Play with popups

---
+++ Src/H.Necessaire/H.Necessaire.Runtime.MAUI/Core/HMauiModalController.cs
@@ -0,0 +1,37 @@
+﻿using H.Necessaire.UI;
+
+namespace H.Necessaire.Runtime.MAUI.Core
+{
+    internal class HMauiModalController : ImAModalController
+    {
+        static Page CurrentPage => Application.Current?.Windows?.FirstOrDefault()?.Page;
+        static INavigation Navigation => CurrentPage?.Navigation;
+
+        readonly TaskCompletionSource<OperationResult<UserOption[]>> modalCompletionSource = new();
+
+        public async Task CancelModal()
+        {
+            INavigation navigation = Navigation;
+            if (navigation is null)
+                return;
+
+            modalCompletionSource.SetResult("User Cancelled");
+            await navigation.PopModalAsync();
+        }
+
+        public async Task SubmitModal(params UserOption[] userSelection)
+        {
+            INavigation navigation = Navigation;
+            if (navigation is null)
+                return;
+
+            modalCompletionSource.SetResult(userSelection);
+            await navigation.PopModalAsync();
+        }
+
+        public Task<OperationResult<UserOption[]>> GetModalResult()
+        {
+            return modalCompletionSource.Task;
+        }
+    }
+}
---
+++ Src/H.Necessaire/H.Necessaire/UI/ImAModal.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace H.Necessaire.UI
+{
+    public interface ImAModal
+    {
+        Task<OperationResult<UserOption[]>> GetModalResult();
+    }
+}
