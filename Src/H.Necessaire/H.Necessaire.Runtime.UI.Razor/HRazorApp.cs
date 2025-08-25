eef035c115ef62c848dec81c94ef20d8c7a879cc
Dan Hintea Qubiz <dan.hintea@qubiz.com>
2025-08-25 12:45:37 +0300

    OD: Blazor UI

--- Src/H.Necessaire/H.Necessaire.Runtime.UI.Razor/HRazorApp.cs
+++ Src/H.Necessaire/H.Necessaire.Runtime.UI.Razor/HRazorApp.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace H.Necessaire.Runtime.UI.Razor
+﻿namespace H.Necessaire.Runtime.UI.Razor
 {
     public class HRazorApp : HApp
     {
         public static readonly new HRazorApp Default = new HRazorApp();
+
+        public HRazorApp()
+        {
+            Branding = HRazorAppBranding.Default;
+        }
     }
 }
---
+++ Src/H.Necessaire/H.Necessaire.Runtime.UI.Razor/IoCExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace H.Necessaire.Runtime.UI.Razor
+{
+    public static class IoCExtensions
+    {
+        public static T WithRazorRuntime<T>(this T dependencyRegistry, HRazorApp hRazorApp = null) where T : ImADependencyRegistry
+        {
+            dependencyRegistry
+                .WithHNecessaireRuntimeUI()
+                //.Register<Core.DependencyGroup>(() => new Core.DependencyGroup())
+                //.Register<Components.DependencyGroup>(() => new Components.DependencyGroup())
+                .Register<HRazorApp>(() => hRazorApp ?? HRazorApp.Default)
+                ;
+
+            return dependencyRegistry;
+        }
+    }
+}
