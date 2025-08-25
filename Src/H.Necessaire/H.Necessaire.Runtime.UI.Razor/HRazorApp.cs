de7598c0983416d8532a71296b8dd0069690b201
Dan Hintea Qubiz <dan.hintea@qubiz.com>
2025-08-25 12:41:39 +0300

    OD: Blazor UI

---
+++ Src/H.Necessaire/H.Necessaire.Runtime.UI.Razor/HRazorApp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.Necessaire.Runtime.UI.Razor
+{
+    public class HRazorApp : HApp
+    {
+        public static readonly new HRazorApp Default = new HRazorApp();
+    }
+}
---
+++ Src/H.Necessaire/H.Necessaire.Runtime.UI.Razor/HRazorAppBranding.cs
@@ -0,0 +1,15 @@
+﻿using H.Necessaire.Models.Branding;
+
+namespace H.Necessaire.Runtime.UI.Razor
+{
+    public class HRazorAppBranding : BrandingStyle
+    {
+        public static readonly new HRazorAppBranding Default = new HRazorAppBranding();
+
+        public override int SizingUnitInPixels => 16;
+
+        public override ColorPalette Colors => HAppColorPalette.LightAzure;
+
+        public override Typography Typography => Typography.Default;
+    }
+}
