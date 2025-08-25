f8ce3f380b6937989f4de5d266ad069f717e9f36
Dan Hintea Qubiz <dan.hintea@qubiz.com>
2025-08-25 14:42:58 +0300

    OD: Blazor UI

--- Src/H.Necessaire/H.Necessaire.Runtime.UI.Razor/HUiToolkit.cs
+++ Src/H.Necessaire/H.Necessaire.Runtime.UI.Razor/HUiToolkit.cs
@@ -4,10 +4,15 @@ namespace H.Necessaire.Runtime.UI.Razor
 {
     public class HUiToolkit : ImADependency
     {
+        RuntimeConfig runtimeConfig;
         HRazorApp hRazorApp;
+        ImAVersionProvider versionProvider;
+        static Version version = null;
         public void ReferDependencies(ImADependencyProvider dependencyProvider)
         {
             hRazorApp = dependencyProvider.Get<HRazorApp>() ?? HRazorApp.Default;
+            runtimeConfig = dependencyProvider.GetRuntimeConfig() ?? RuntimeConfig.Empty;
+            versionProvider = dependencyProvider.Get<ImAVersionProvider>();
         }
 
         public HRazorApp App => hRazorApp ?? HRazorApp.Default;
@@ -15,7 +20,14 @@ namespace H.Necessaire.Runtime.UI.Razor
         public int SizingUnit => App?.Branding?.SizingUnitInPixels ?? HRazorAppBranding.Default.SizingUnitInPixels;
         public T Get<T>() => App.DependencyRegistry.Get<T>();
         public T Build<T>(string id) where T : class => App.DependencyRegistry.Build<T>(id);
+        public RuntimeConfig Config => runtimeConfig;
 
-        
+        public async Task<Version> GetAppVersion()
+        {
+            version ??= versionProvider is null ? Version.Unknown : await versionProvider.GetCurrentVersion();
+
+            return version;
+        }
+
     }
 }
