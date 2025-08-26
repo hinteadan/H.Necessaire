6b9bc076b733e5af74d4b3d320f73c71d14ad90e
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2025-08-26 11:04:41 +0300

    OD: Blazor UI

--- Src/H.Necessaire/H.Necessaire.Runtime.UI.Razor/wwwroot/h.js
+++ Src/H.Necessaire/H.Necessaire.Runtime.UI.Razor/wwwroot/h.js
@@ -1,20 +1,19 @@
 // This is a JavaScript module that is loaded on demand. It can export any number of
 // functions, and may import other JavaScript modules if required.
 
-//class H {
-//    static Instance = new H();
-//    constructor() {
+class H {
+    static Instance = new H();
+    constructor() {
 
-//    }
+    }
 
-//    Debug(test) {
-//        console.log('Log from h.js');
-//        console.log(test);
-//    }
-//}
+    Debug(test) {
+        console.log('Log from h.js');
+        console.log(test);
+    }
+}
 
 
 export function Debug(test) {
-    return prompt("test", 'Type anything here');
-    //H.Instance.Debug(test);
+    H.Instance.Debug(test);
 }
