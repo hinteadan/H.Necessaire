b0d4203b287c652546b9616f9f88b4f85acad91f
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2025-08-28 10:21:35 +0300

    OD: Blazor UI

--- Src/H.Necessaire/H.Necessaire/Operations/ImAnHHttpService.cs
+++ Src/H.Necessaire/H.Necessaire/Operations/ImAnHHttpService.cs
@@ -1,11 +1,12 @@
 ﻿using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace H.Necessaire
 {
     public interface ImAnHHttpService
     {
-        Task<OperationResult<HsHttpFullResponse>> DoFullRequestResponse(HttpRequestMessage httpRequestMessage);
-        Task<OperationResult<HsHttpStreamResponse>> DoStreamedRequestResponse(HttpRequestMessage httpRequestMessage);
+        Task<OperationResult<HsHttpFullResponse>> DoFullRequestResponse(HttpRequestMessage httpRequestMessage, CancellationToken? cancellationToken = null);
+        Task<OperationResult<HsHttpStreamResponse>> DoStreamedRequestResponse(HttpRequestMessage httpRequestMessage, CancellationToken? cancellationToken = null);
     }
 }
