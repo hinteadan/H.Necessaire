b327d5095fb72d2071f898ec77fc9d720517ff49
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2025-12-17 14:59:47 +0200

    v3.0.1 Small Tweaks on Blazor Runtime and create pack fpor Razor UI Runtime

---
+++ Src/H.Necessaire/Apps/H.Necessaire.AspNetCoreWebAppSample/Controllers/AppSampleTestController.cs
@@ -0,0 +1,15 @@
+﻿using H.Necessaire.Runtime.Integration.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace H.Necessaire.AspNetCoreWebAppSample.Controllers
+{
+    [Route(HAspNetCoreConstants.ApiControllerBaseRoute)]
+    [ApiController]
+    public class AppSampleTestController : ControllerBase
+    {
+        [Route("ping"), HttpGet]
+        public Task<string> Pong() => $"AppSampleTestController.Pong @ {DateTime.Now.PrintDateAndTime()}".AsTask();
+    }
+}
--- Src/H.Necessaire/H.Necessaire.Runtime.Integration.AspNetCore/WebAppWireupExtensions.cs
+++ Src/H.Necessaire/H.Necessaire.Runtime.Integration.AspNetCore/WebAppWireupExtensions.cs
@@ -1,12 +1,33 @@
 ﻿using H.Necessaire.Runtime.Integration.AspNetCore.Middlewares;
+using H.Necessaire.Runtime.Integration.DotNet;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace H.Necessaire.Runtime.Integration.AspNetCore
 {
-    internal static class WebAppWireupExtensions
+    public static class WebAppWireupExtensions
     {
+        public static THostApplicationBuilder WithHNecessaire<THostApplicationBuilder>(this THostApplicationBuilder hostApplicationBuilder, ImADependencyRegistry dependencyRegistry)
+            where THostApplicationBuilder : IHostApplicationBuilder
+        {
+            hostApplicationBuilder
+                .Services
+                .AddHttpContextAccessor()
+                .WithHNecessaireAspNetRuntime(dependencyRegistry, hostApplicationBuilder.Configuration)
+                ;
+
+            hostApplicationBuilder
+                .Logging
+                .ClearProviders()
+                .AddHNecessaireLogging(dependencyRegistry)
+                ;
+
+            return hostApplicationBuilder;
+        }
+
         public static TApplicationBuilder ConfigureHNecessaireAspNetRuntime<TApplicationBuilder>(this TApplicationBuilder applicationBuilder, IHostEnvironment hostEnvironment) where TApplicationBuilder : IApplicationBuilder
         {
             applicationBuilder.Use((ctx, next) => { ctx.Request.EnableBuffering(); return next(); });
