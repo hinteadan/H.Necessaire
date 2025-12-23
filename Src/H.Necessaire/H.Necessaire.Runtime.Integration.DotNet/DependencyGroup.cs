6766e9ac375a435501ab25042a5339d23d8e8336
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2025-12-23 14:47:38 +0200

    v3.0.7 Tweak DotNetRuntime + Update RavenDB Client Nuget

--- Src/H.Necessaire/H.Necessaire.Runtime.Integration.DotNet/DependencyGroup.cs
+++ Src/H.Necessaire/H.Necessaire.Runtime.Integration.DotNet/DependencyGroup.cs
@@ -13,9 +13,11 @@ namespace H.Necessaire.Runtime.Integration.DotNet
 
         public void RegisterDependencies(ImADependencyRegistry dependencyRegistry)
         {
+            bool isRuntimeDependencyGroupAlreadyRegistered = dependencyRegistry.Get<RuntimeDependencyGroup>() != null;
+
             RuntimeConfig runtimeConfig = dependencyRegistry.GetRuntimeConfig();
             dependencyRegistry
-                .Register<RuntimeDependencyGroup>(() => new RuntimeDependencyGroup())
+                .AndIf(!isRuntimeDependencyGroupAlreadyRegistered, x => x.Register<RuntimeDependencyGroup>(() => new RuntimeDependencyGroup()))
                 .Register<NetCoreLoggerProvider>(() => new NetCoreLoggerProvider())
                 .Register<ImAUseCaseContextProvider>(() => new Concrete.DotNetEnvironmentUseCaseContextProvider())
                 .Register<ImAConfigProvider>(() => new Concrete.NetCoreConfigProvider(runtimeConfig, configuration))
