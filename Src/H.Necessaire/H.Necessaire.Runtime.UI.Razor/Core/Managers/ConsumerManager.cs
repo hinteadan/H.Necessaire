5211bfe57f449ce4a8d5c68cf826cd140192a36d
Dan Hintea Qubiz <dan.hintea@qubiz.com>
2026-04-24 16:41:32 +0300

    Fix Razor UI

--- Src/H.Necessaire/H.Necessaire.Runtime.UI.Razor/Core/Managers/ConsumerManager.cs
+++ Src/H.Necessaire/H.Necessaire.Runtime.UI.Razor/Core/Managers/ConsumerManager.cs
@@ -24,7 +24,7 @@ namespace H.Necessaire.Runtime.UI.Razor.Core.Managers
             currentConsumerIdentity = consumerIdentity;
             HIndexedDbContext dbContext = hIndexedDbContextProvider();
             IndexedDbStore consumerIdentityStore = dbContext.CoreDatabase[nameof(ConsumerIdentity)];
-            bool isOK = await consumerIdentityStore.StoreAsync(consumerIdentity);
+            bool isOK = (await consumerIdentityStore.StoreItemAsync<IndexedDbStoreEntity<ConsumerIdentity, Guid>>(consumerIdentity)) != null;
 
             if (isOK)
             {
@@ -61,7 +61,7 @@ namespace H.Necessaire.Runtime.UI.Razor.Core.Managers
         {
             HIndexedDbContext dbContext = hIndexedDbContextProvider();
             IndexedDbStore consumerIdentityStore = dbContext.CoreDatabase[nameof(ConsumerIdentity)];
-            IAsyncEnumerable<ConsumerIdentity> allConsumers = consumerIdentityStore.GetAllAsync<ConsumerIdentity>();
+            IAsyncEnumerable<ConsumerIdentity> allConsumers = consumerIdentityStore.GetAllAsync<IndexedDbStoreEntity<ConsumerIdentity, Guid>>().Select(x => x.Data);
             ConsumerIdentity consumerIdentity = await allConsumers.OrderByDescending(x => x.AsOf).FirstOrDefaultAsync();
             return consumerIdentity;
         }
---
+++ Src/H.Necessaire/H.Necessaire.Runtime.UI.Razor/Core/Storage/IndexedDbStoreEntity.cs
@@ -0,0 +1,16 @@
+﻿using Tavenem.DataStorage;
+
+namespace H.Necessaire.Runtime.UI.Razor.Core.Storage
+{
+    internal class IndexedDbStoreEntity<T, TID> : IIdItem where T : IDentityType<TID>
+    {
+        public string Id => Data?.ID?.ToString();
+
+        public string IdItemTypeName { get; init; } = typeof(T).Name;
+
+        public T Data { get; set; }
+
+        public static implicit operator T(IndexedDbStoreEntity<T, TID> x) => x.Data;
+        public static implicit operator IndexedDbStoreEntity<T, TID>(T x) => new IndexedDbStoreEntity<T, TID> { Data = x };
+    }
+}
