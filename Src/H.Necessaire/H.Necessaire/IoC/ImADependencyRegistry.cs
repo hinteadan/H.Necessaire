14d2ef572f2be9a392dcceab4e5358f4143621e2
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2026-05-01 11:29:49 +0300

    v3.1.5 Add typedFactory to DI API; Update NuGets

--- Src/H.Necessaire/H.Necessaire/IoC/ImADependencyRegistry.cs
+++ Src/H.Necessaire/H.Necessaire/IoC/ImADependencyRegistry.cs
@@ -5,10 +5,14 @@ namespace H.Necessaire
     public interface ImADependencyRegistry : ImADependencyProvider, ImADependencyBrowser
     {
         ImADependencyRegistry Register<T>(Func<object> factory);
+        ImADependencyRegistry Register<T>(Func<Type, object> factory);
         ImADependencyRegistry Register(Type type, Func<object> factory);
+        ImADependencyRegistry Register(Type type, Func<Type, object> typedFactory);
 
         ImADependencyRegistry RegisterAlwaysNew<T>(Func<object> factory);
+        ImADependencyRegistry RegisterAlwaysNew<T>(Func<Type, object> factory);
         ImADependencyRegistry RegisterAlwaysNew(Type type, Func<object> factory);
+        ImADependencyRegistry RegisterAlwaysNew(Type type, Func<Type, object> typedFactory);
 
         ImADependencyRegistry Unregister<T>();
         ImADependencyRegistry Unregister(Type type);
