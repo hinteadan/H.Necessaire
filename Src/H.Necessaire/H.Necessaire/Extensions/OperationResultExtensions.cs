770117dd184c8d63fb9bda60eef426f33ae2f32c
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2026-06-04 10:18:28 +0300

    v3.4.0 Improve TrimToValidKeywordsOnly; Added UnwrapToFirstFailOrLastWin and DeepUnwrapToFirstFailOrLastWin for OperationResults; Update NuGets

--- Src/H.Necessaire/H.Necessaire/Extensions/OperationResultExtensions.cs
+++ Src/H.Necessaire/H.Necessaire/Extensions/OperationResultExtensions.cs
@@ -29,5 +29,65 @@ namespace H.Necessaire
 
             return operationResult;
         }
+
+        public static OperationResult UnwrapToFirstFailOrLastWin(this OperationResult<OperationResult> opRes)
+        {
+            if (opRes == null)
+                return opRes;
+
+            if (!opRes.IsSuccessful)
+                return opRes;
+
+            return opRes.Payload;
+        }
+
+        public static OperationResult<T> UnwrapToFirstFailOrLastWin<T>(this OperationResult<OperationResult<T>> opRes)
+        {
+            if (opRes == null)
+                return opRes?.WithoutPayload<T>();
+
+            if (!opRes.IsSuccessful)
+                return opRes.WithoutPayload<T>();
+
+            return opRes.Payload;
+        }
+
+        public static OperationResult DeepUnwrapToFirstFailOrLastWin<T> (this OperationResult<T> opRes) where T : OperationResult
+        {
+            if (opRes == null)
+                return opRes;
+
+            if (!opRes.IsSuccessful)
+                return opRes;
+
+            var innerOpRes = opRes.Payload;
+
+            if (innerOpRes == null)
+                return innerOpRes;
+
+            if (innerOpRes is OperationResult<T> deeperOpRes)
+                return deeperOpRes.DeepUnwrapToFirstFailOrLastWin();
+
+            return innerOpRes;
+        }
+
+        public static OperationResult<TResult> DeepUnwrapToFirstFailOrLastWin<T, TResult>(this OperationResult<T> opRes) where T : OperationResult<TResult>
+        {
+            if (opRes == null)
+                return opRes.WithoutPayload<TResult>();
+
+            if (!opRes.IsSuccessful)
+                return opRes.WithoutPayload<TResult>();
+
+            var innerOpRes = opRes.Payload;
+
+            if (innerOpRes == null)
+                return innerOpRes.WithoutPayload<TResult>();
+
+            if (innerOpRes is OperationResult<T> deeperOpRes)
+                return deeperOpRes.DeepUnwrapToFirstFailOrLastWin<T, TResult>();
+
+            return innerOpRes;
+        }
     }
 }
