c148b898a03c8f4d93caa4104301ca3bf2157441
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2026-07-20 12:00:02 +0300

    v3.5.0 Implement BackgroundServiceDaemon; Update NuGets;

---
+++ Src/H.Necessaire/H.Necessaire.Runtime/Daemons/BackgroundServiceDaemonBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace H.Necessaire.Runtime
+{
+    public abstract class BackgroundServiceDaemonBase : ImADaemon, ImADependency
+    {
+        BackgroundServiceDaemon backgroundServiceDaemon;
+        public virtual void ReferDependencies(ImADependencyProvider dependencyProvider)
+        {
+            backgroundServiceDaemon = new BackgroundServiceDaemon(dependencyProvider, DoWork, DoWorkInterval, StartDelay);
+        }
+        protected abstract TimeSpan DoWorkInterval { get; }
+        protected abstract Task DoWork();
+
+        protected virtual TimeSpan? StartDelay => null;
+
+        public virtual async Task Start(CancellationToken? cancellationToken = null)
+            => await backgroundServiceDaemon.Start(cancellationToken);
+
+        public virtual async Task Stop(CancellationToken? cancellationToken = null)
+            => await backgroundServiceDaemon.Stop(cancellationToken);
+    }
+}
