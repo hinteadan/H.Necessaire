825bd4f83115176e29b5ffd67c6c79fce3104722
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2026-09-14 19:57:16 +0300

    v3.7.6 Fix HCounter thread safety; Update Deps

--- Src/H.Necessaire/H.Necessaire/Models/Metrics/HCounter.cs
+++ Src/H.Necessaire/H.Necessaire/Models/Metrics/HCounter.cs
@@ -5,26 +5,35 @@ namespace H.Necessaire
     public class HCounter : IStringIdentity
     {
         public string ID { get; set; }
-        public long Count { get; set; }
+        long count;
+        public long Count { get => count; set => Interlocked.Exchange(ref count, value); }
 
         public HCounter Increment(long by = 1)
         {
-            long count = Count;
+            if (by == 0)
+                return this;
 
-            Count = by == 1 ? Interlocked.Increment(ref count) : Interlocked.Add(ref count, by);
-
-            Count = count;
+            if (by == 1)
+                Interlocked.Increment(ref count);
+            else if (by == -1)
+                Interlocked.Decrement(ref count);
+            else
+                Interlocked.Add(ref count, by);
 
             return this;
         }
 
         public HCounter Decrement(long by = 1)
         {
-            long count = Count;
+            if (by == 0)
+                return this;
 
-            Count = by == 1 ? Interlocked.Decrement(ref count) : Interlocked.Add(ref count, -by);
-
-            Count = count;
+            if (by == 1)
+                Interlocked.Decrement(ref count);
+            else if (by == -1)
+                Interlocked.Increment(ref count);
+            else
+                Interlocked.Add(ref count, -by);
 
             return this;
         }
