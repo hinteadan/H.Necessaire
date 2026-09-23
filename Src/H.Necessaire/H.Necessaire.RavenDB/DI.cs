6fc1431532cfab95015132ccc2f23a2db2e1f9c3
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2026-09-23 23:55:38 +0300

    v3.8.1 Add DI extensions for RavenDB DocumentStore

---
+++ Src/H.Necessaire/H.Necessaire.RavenDB/DI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace H.Necessaire.RavenDB
+{
+    public static class DI
+    {
+        public static RavenDbDocumentStore GetDefaultRavenDbDocumentStore(this ImADependencyProvider dependencyProvider)
+            => dependencyProvider?.Get<RavenDbDocumentStore>();
+
+        public static RavenDbDocumentStore NewRavenDbDocumentStore(this ImADependencyProvider dependencyProvider, Func<RuntimeConfig, ConfigNode> ravenDbConfigRootNodeProvider)
+            => new RavenDbDocumentStore(ravenDbConfigRootNodeProvider).And(x => x.ReferDependencies(dependencyProvider));
+        public static RavenDbDocumentStore NewRavenDbDocumentStore(this ImADependencyProvider dependencyProvider, Func<ConfigNode> ravenDbConfigRootNodeProvider)
+            => new RavenDbDocumentStore(_ => ravenDbConfigRootNodeProvider?.Invoke()).And(x => x.ReferDependencies(dependencyProvider));
+        public static RavenDbDocumentStore NewRavenDbDocumentStore(this ImADependencyProvider dependencyProvider, ConfigNode ravenDbConfigRootNode)
+            => new RavenDbDocumentStore(_ => ravenDbConfigRootNode).And(x => x.ReferDependencies(dependencyProvider));
+        public static RavenDbDocumentStore NewRavenDbDocumentStore(this ImADependencyProvider dependencyProvider)
+            => new RavenDbDocumentStore().And(x => x.ReferDependencies(dependencyProvider));
+    }
+}
